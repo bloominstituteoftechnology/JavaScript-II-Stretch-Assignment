07cfe6596f4abff95d3d763a20aa24867df90969
SunJieMing <bennelson@Bens-MacBook-Pro.local>
2017-10-04 12:41:49 -0700

    Initial commit

---
+++ src/recursion.js
@@ -0,0 +1,25 @@
+// Complete the following functions.
+
+const nFibonacci = (n) => {
+  // fibonacci sequence: 1 2 3 5 8 13 ...
+  // return the nth number in the sequence
+};
+
+const nFactorial = (n) => {
+  // factorial example: !5 = 5 * 4 * 3 * 2 * 1
+  // return the factorial of `n`
+};
+
+/* Extra Credit */
+const checkMatchingLeaves = (obj) => {
+  // return true if every property on `obj` is the same
+  // otherwise return false
+};
+
+/* eslint-enable no-unused-vars */
+
+module.exports = {
+  nFibonacci,
+  nFactorial,
+  checkMatchingLeaves,
+};
