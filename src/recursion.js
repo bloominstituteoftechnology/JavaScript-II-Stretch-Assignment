e461a867ac5a6d6e74a927803acba2ec802d49ba
Aaron Artea <aarongitis@gmail.com>
2017-10-05 13:40:04 -0700

    finish recursion.js, minus e.c.

--- src/recursion.js
+++ src/recursion.js
@@ -3,17 +3,27 @@
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return nFibonacci(n - 2) + nFibonacci(n - 1);
 };
 
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * nFactorial(n - 1);
 };
 
 /* Extra Credit */
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  // return checkMatchingLeaves(obj.property) == checkMatchingLeaves(obj.property);
+  // return Object.prototype.hasOwnProperty.call(obj, obj.property);
 };
 
 /* eslint-enable no-unused-vars */
