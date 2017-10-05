8bb0e2d6fce8755393b1328d0a57c8765024b7aa
MannieJay <jhall02258@gmail.com>
2017-10-05 18:47:57 -0400

    Final class.js

--- src/class.js
+++ src/class.js
@@ -9,17 +9,40 @@
 
 // code here
 
+class User {
+  constructor(options) {
+    this.email = options.email;
+    this.password = options.password;
+  }
+  comparePasswords(currentPassword) {
+    return this.password === currentPassword;
+  }
+}
+
 // Part 2
 // Create a class called `Animal` and a class called `Cat` using ES6 classes.
 // `Cat` should extend the `Animal` class.
 // Animal and Cat should both have a parameter called `options` in their constructors.
 // Animal should have the property `age` that's set in the constructor and the method
-// `growOlder` that returns the age.
+// `growOlder` that returns the age. !!! PLEASE BE MORE SPECIFIC ABOUT INCEMENTING THIS!!!
 // Cat should have the property `name` that is set in the constructor and the method
 // `meow` that should return the string `<name> meowed!` where `<name>` is the `name`
 // property set on the Cat instance.
 
 // code here
+class Animal {
+  constructor(options) {
+    this.age = options.age;
+  }
+  growOlder() { return ++this.age; }
+}
+class Cat extends Animal {
+  constructor(options) {
+    super(options);
+    this.name = options.name;
+  }
+  meow() { return `${this.name} meowed!`; }
+}
 
 /* eslint-disable no-undef */
 
