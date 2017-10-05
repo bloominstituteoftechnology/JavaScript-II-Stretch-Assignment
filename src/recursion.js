5f65410dd175ebd8571022e0d067b11844c30f43
Gary Dillard <thomas.dillard.studios@gmail.com>
2017-10-05 16:11:24 -0700

    Updated checkMatchingLeaves to more eloquent JavaScript.

--- src/recursion.js
+++ src/recursion.js
@@ -8,8 +8,6 @@ const nFibonacci = (n) => {
   return nFibonacci(n - 1) + nFibonacci(n - 2);
 };
 
-nFibonacci(5);
-
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
@@ -22,25 +20,16 @@ const nFactorial = (n) => {
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
-  const check = (objPass) => {
-    const arr = [];
-    Object.keys(objPass).forEach((key) => {
-      if (typeof objPass[key] === 'object') {
-        const flatObj = check(objPass[key]);
-        Object.keys(flatObj).forEach((secKey) => {
-          arr.push(flatObj[secKey]);
-        });
-      } else {
-        arr.push(objPass[key]);
+  const lightFlatten = arr => [].concat(...arr);
+  const heavyFlatten = (object) => {
+    return lightFlatten(Object.values(object).map((element) => {
+      if (typeof element === 'object') {
+        return heavyFlatten(element);
       }
-    });
-    return arr;
+      return element;
+    }));
   };
-  const arr = check(obj);
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== arr[i + 1] && i + 1 !== arr.length) return false;
-  }
-  return true;
+  return new Set(heavyFlatten(obj)).size === 1;
 };
 
 /* eslint-enable no-unused-vars */
