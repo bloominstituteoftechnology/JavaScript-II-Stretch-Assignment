f87c05d018e8cd2d973c6793187ce2a82aa5a406
sulemc <32285312+sulemc@users.noreply.github.com>
2017-10-05 16:07:29 -0500

    Initial commit

--- src/prototype.js
+++ src/prototype.js
@@ -51,8 +51,40 @@
 
 /* eslint-disable no-undef */
 
-module.exports = {
-  GameObject,
-  NPC,
-  Humanoid,
-};
+ function GameObject(options) {
+   this.createdAt = options.createdAt;
+   this.dimensions = options.dimensions;
+ }
+ GameObject.prototype.destroy = function () {
+   return 'Game object was removed from the game.';
+ };
+ function NPC(options) {
+   this.createdAt = options.createdAt;
+   this.dimensions = options.dimensions;
+   this.hp = options.hp;
+   this.name = options.name;
+ }
+ NPC.prototype = Object.create(GameObject.prototype);
+ NPC.prototype.takeDamage = function () {
+   return `${this.name} took damage.`;
+ };
+ function Humanoid(options) {
+   this.createdAt = options.createdAt;
+   this.dimensions = options.dimensions;
+   this.hp = options.hp;
+   this.name = options.name;
+   this.faction = options.faction;
+   this.weapons = options.weapons;
+   this.language = options.language;
+ }
+ Humanoid.prototype = Object.create(NPC.prototype);
+ Humanoid.prototype.greet = function () {
+   return `${this.name} offers a greeting in ${this.language}.`;
+ };
+
+
+ module.exports = {
+   GameObject,
+   NPC,
+   Humanoid,
+ };
