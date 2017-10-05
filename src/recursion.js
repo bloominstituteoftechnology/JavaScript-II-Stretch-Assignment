b4a0667d3063e3d00893dc110cf9900254123f8a
Jesse Reichel <jesse.reichel@live.com>
2017-10-05 14:27:53 -0700

    problem progress

--- src/recursion.js
+++ src/recursion.js
@@ -1,19 +1,26 @@
 // Complete the following functions.
 
+
+// creat a loop that runs the fibonacci sequence terminating on the n value passed through nFibonacci
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  if (n <= 1) return 1;
+  return nFibonacci(n - 1) + nFibonacci(n - 2);
 };
 
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+  if (n === 0) return 1;
+  return n * nFactorial(n - 1);
 };
 
 /* Extra Credit */
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  Object.keys(obj).map()
 };
 
 /* eslint-enable no-unused-vars */
