47fca76e7f53487e570eb57c330dfb0fe5e41c2a
Cassidy SN Avery <tacolim@gmail.com>
2017-10-05 14:22:58 -0400

    prototype gameobject passes

--- src/prototype.js
+++ src/prototype.js
@@ -49,6 +49,40 @@
   hamsterHuey.destroy(); // returns 'Game object was removed from the game.'
 */
 
+function GameObject(options) {
+  this.createdAt = options.createdAt;
+  this.dimensions = options.dimensions;
+}
+
+GameObject.prototype.destroy = function () {
+  return 'Game object was removed from the game.';
+};
+
+function NPC(options) {
+  GameObject.call(this, options.createdAt, options.dimensions);
+  this.hp = options.hp;
+  this.name = options.name;
+}
+
+NPC.prototype = Object.create(GameObject.prototype);
+
+NPC.prototype.takeDamange = function () {
+  return `${this.name} took damage.`;
+};
+
+function Humanoid(options) {
+  NPC.call(this, options.createdAt, options.dimensions, options.hp, options.name);
+  this.faction = options.faction;
+  this.weapons = options.weapons;
+  this.language = options.language;
+}
+
+Humanoid.prototype = Object.create(NPC.prototype);
+
+Humanoid.prototype.greet = function () {
+  return `${this.name} offers a greeting in ${this.language}.`;
+};
+
 /* eslint-disable no-undef */
 
 module.exports = {
