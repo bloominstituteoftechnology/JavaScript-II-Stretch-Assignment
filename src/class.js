440bd17d0a41a2af3df1c31de7d9a2b0cf848680
BonnW <pwonghansa@gmail.com>
2017-10-05 18:42:20 -0500

    nFactorial test passed

--- src/class.js
+++ src/class.js
@@ -8,6 +8,7 @@
 // Return true if the potential password matches the `password` property.  Otherwise return false.
 
 // code here
+const User = {};
 
 // Part 2
 // Create a class called `Animal` and a class called `Cat` using ES6 classes.
