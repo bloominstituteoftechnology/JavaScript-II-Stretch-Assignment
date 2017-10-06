e4ea40f143188f30ed80b6101886dbe128191778
MannieJay <jhall02258@gmail.com>
2017-10-05 20:17:15 -0400

    Final recursion.js

--- src/recursion.js
+++ src/recursion.js
@@ -3,20 +3,24 @@
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  if (n < 2) return 1;
+  return nFibonacci(n - 1) + nFibonacci(n - 2);
 };
 
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+  if (n === 1) return 1;
+  return n * nFactorial(n - 1);
 };
 
 /* Extra Credit */
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
-};
 
-/* eslint-enable no-unused-vars */
+  
+  /* eslint-enable no-unused-vars */
 
 module.exports = {
   nFibonacci,
