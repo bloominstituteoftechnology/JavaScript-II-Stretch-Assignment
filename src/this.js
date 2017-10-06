b3cabcdaa1649288c50b3ce9a1e5828e217b2965
Sam Kim <sams404link@gmail.com>
2017-10-06 16:28:01 -0700

    JavaSCript-II this.js commit

--- src/this.js
+++ src/this.js
@@ -35,13 +35,19 @@ const checkPassword = function comparePasswords(passwordToCompare) {
   // use `this` to access the object's `password` property.
   // do not modify this function's parameters
   // note that we use the `function` keyword and not `=>`
+
+  if (me.password === passwordToCompare) {
+    return true;
+  } 
+  return false; 
 };
 
 // invoke `checkPassword` on `me` by explicitly setting the `this` context
 // use .call, .apply, and .bind
 
 // .call
-
+checkPassword.call(me);
 // .apply
-
+checkPassword.apply(me);
 // .bind
+const boundPassword = checkPassword.bind(me);
