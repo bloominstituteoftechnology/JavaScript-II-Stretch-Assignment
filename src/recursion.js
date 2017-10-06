0551ab121985e019d044c03c5eb467457df8693a
BonnW <pwonghansa@gmail.com>
2017-10-06 17:26:01 -0500

    Recursion finished except Extra Credit

--- src/recursion.js
+++ src/recursion.js
@@ -3,6 +3,8 @@
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  if (n < 2) return 1;
+  return nFibonacci(n - 1) + nFibonacci(n - 2);
 };
 
 const nFactorial = (n) => {
