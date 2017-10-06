94c2ba563c2de5913ebc25c24a5e17ccba2a3d8a
sulemc <32285312+sulemc@users.noreply.github.com>
2017-10-06 16:45:25 -0500

    all but the EC

--- src/this.js
+++ src/this.js
@@ -7,11 +7,15 @@
 class User {
   constructor(options) {
     // set a username and password property on the user object that is created
-  }
+    this.username = options.username;
+    this.password = options.password;
+  };
   // create a method on the User class called `checkPassword`
   // this method should take in a string and compare it to the object's password property
   // return `true` if they match, otherwise return `false`
 }
+  User.prototype.checkPassword = function(potentialPassword){
+  return this.password === potentialPassword;}
 
 const me = new User({
   username: 'LambdaSchool',
@@ -20,6 +24,7 @@ const me = new User({
 
 const result = me.checkPassword('correcthorsebatterystaple'); // should return `true`
 
+
 /* part 2 */
 
 const checkPassword = function comparePasswords(passwordToCompare) {
@@ -27,13 +32,16 @@ const checkPassword = function comparePasswords(passwordToCompare) {
   // use `this` to access the object's `password` property.
   // do not modify this function's parameters
   // note that we use the `function` keyword and not `=>`
+ return this.User.password === passwordToCompare;
 };
 
+function comparePasswords(me) { return this.password; }
 // invoke `checkPassword` on `me` by explicitly setting the `this` context
 // use .call, .apply, and .bind
 
-// .call
-
+// .call 
+comparePasswords.call(me);
 // .apply
-
+comparePasswords.apply(me);
 // .bind
+const boundCheck = comparePasswords.bind(me);
