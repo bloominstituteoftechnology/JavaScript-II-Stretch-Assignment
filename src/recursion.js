266a1056ec09e9bb169724a6589ba511f2e8bb44
Ellen Nitchals <ellen.nitchals@gmail.com>
2017-10-08 13:26:16 -0700

    all complete except checkMatchingLeaves

--- src/recursion.js
+++ src/recursion.js
@@ -1,11 +1,7 @@
 // Complete the following functions.
 
 const nFibonacci = (n) => {
-  if (n - 1 === 0) {
-    return 1;
-  } if (n - 1 === 1) {
-    return 2;
-  }
+  if (n < 2) { return 1; }
   return nFibonacci(n - 1) + nFibonacci(n - 2);
 };
   // fibonacci sequence: 1 2 3 5 8 13 ...
@@ -20,18 +16,27 @@ const nFactorial = (n) => {
   return n * nFactorial(n - 1);
 };
 
-/* Extra Credit */
-const checkMatchingLeaves = (obj) => {
+/* Extra Credit
+const checkMatchingLeaves = (obj) => { */
   // return true if every property on `obj` is the same
   // otherwise return false
+/*
+  const values = (obj) => {
+    return Object.values(obj);
+    for (let i = 0; i < object.values.length(obj); i++) {
+      if typeOf obj.values[i] = "object" {
+        values(obj[i]);
+      }
+    }
+  }
   const values = Object.values(obj);
-//  if typeof Object.values[i] = 'object,' // this was from a thread with Thomas & Kia -- not sure where to go next.
 };
+*/
 
 /* eslint-enable no-unused-vars */
 
 module.exports = {
   nFibonacci,
   nFactorial,
-  checkMatchingLeaves,
+  // checkMatchingLeaves,
 };
--- src/this.js
+++ src/this.js
@@ -24,23 +24,32 @@ const me = new User({
 });
 
 const result = me.checkPassword('correcthorsebatterystaple'); // should return `true`
+console.log(result);
 
 /* part 2 */
-
-const checkPassword = function comparePasswords(passwordToCompare) {
-  User.call(this, options);
-  return 
   // recreate the `checkPassword` method that you made on the `User` class
   // use `this` to access the object's `password` property.
   // do not modify this function's parameters
   // note that we use the `function` keyword and not `=>`
+
+const checkPassword = function comparePasswords(passwordToCompare) {
+  return (this.password === passwordToCompare);
 };
 
 // invoke `checkPassword` on `me` by explicitly setting the `this` context
 // use .call, .apply, and .bind
 
 // .call
+const callTrue = checkPassword.call(me, 'correcthorsebatterystaple'); // should return 'true'
+const callFalse = checkPassword.call(me, 'wrong password'); // should return 'false'
+console.log(callTrue, callFalse);
 
 // .apply
+const applyTrue = checkPassword.apply(me, ['correcthorsebatterystaple']); // should return 'true'
+const applyFalse = checkPassword.apply(me, ['wrong password']); // should return 'false'
+console.log(applyTrue, applyFalse);
 
 // .bind
+const bindTrue = checkPassword.bind(me)('correcthorsebatterystaple'); // should return 'true'
+const bindFalse = checkPassword.bind(me)('wrongpassword'); // should return 'false'
+console.log(bindTrue, bindFalse);
