681ee16c9b2931294b43ef049e780bb2eb0db337
ubuntu <irishseanvaldivia@gmail.com>
2017-10-08 16:48:03 -0500

    Here we go...

--- src/recursion.js
+++ src/recursion.js
@@ -1,13 +1,17 @@
 // Complete the following functions.
-
+// fibonacci sequence: 1 2 3 5 8 13 ...
+// return the nth number in the sequence
 const nFibonacci = (n) => {
-  // fibonacci sequence: 1 2 3 5 8 13 ...
-  // return the nth number in the sequence
+  if (n < 2) return 1;
+  return nFibonacci(n - 2) + nFibonacci(n - 1);
 };
 
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+  if (n === 0) {
+    return 1;
+  } return n * nFactorial(n - 1);
 };
 
 /* Extra Credit */
--- src/this.js
+++ src/this.js
@@ -7,10 +7,15 @@
 class User {
   constructor(options) {
     // set a username and password property on the user object that is created
+    this.email = options.email;
+    this.password = options.password;
   }
   // create a method on the User class called `checkPassword`
   // this method should take in a string and compare it to the object's password property
   // return `true` if they match, otherwise return `false`
+  checkPassword(string) {
+    return (string === this.password);
+  }
 }
 
 const me = new User({
@@ -19,6 +24,7 @@ const me = new User({
 });
 
 const result = me.checkPassword('correcthorsebatterystaple'); // should return `true`
+console.log(result);
 
 /* part 2 */
 
@@ -27,13 +33,16 @@ const checkPassword = function comparePasswords(passwordToCompare) {
   // use `this` to access the object's `password` property.
   // do not modify this function's parameters
   // note that we use the `function` keyword and not `=>`
+  return (passwordToCompare === this.password);
 };
 
 // invoke `checkPassword` on `me` by explicitly setting the `this` context
 // use .call, .apply, and .bind
 
 // .call
-
+console.log(checkPassword.call(me, 'correcthorsebatterystaple'));
 // .apply
-
+console.log(checkPassword.apply(me, ['correcthorsebatterystaple']));
 // .bind
+const checkedPassword = checkPassword.bind(me);
+console.log(checkedPassword('correcthorsebatterystaple'));
