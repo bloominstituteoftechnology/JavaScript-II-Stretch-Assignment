06cd3b2cb4611f44ff8ef189e5d8deb77ab0030a
Biff <thedonnieutley@gmail.com>
2017-10-08 10:25:19 -0500

    all problems passed tests

--- src/recursion.js
+++ src/recursion.js
@@ -20,6 +20,23 @@ const nFactorial = (n) => {
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+
+  // Basically reused flattenedArr from previous work.
+  const flatten = (elements) => {
+    const flattenedArr = Object.values(elements).reduce((memo, item) => {
+      if (typeof item === 'object') return memo.concat(flatten(item));
+      return memo.concat(item);
+    }, []);
+    return flattenedArr;
+  };
+
+  // Attempted solution w/o use of flatten(obj) but wasn't able to test
+  // flattened array and return a boolean w/o extra function.
+  const flatArr = flatten(obj);
+  if (!flatArr.every(element => element === flatArr[0])) {
+    return false;
+  }
+  return true;
 };
 
 /* eslint-enable no-unused-vars */
