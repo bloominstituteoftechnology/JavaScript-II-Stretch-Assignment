d16eda53230cff40d8b49abdd8f87665d5a3034c
Alex Cassell <alex@simulism.net>
2017-10-08 14:22:15 -0400

    almost done

--- src/this.js
+++ src/this.js
@@ -7,15 +7,16 @@
 class User {
   constructor(options) {
     // set a username and password property on the user object that is created
-    this.username = options.username;
+    this.username = options.username; 
     this.password = options.password;
   }
-  // create a method on the User class called `checkPassword`
-  // this method should take in a string and compare it to the object's password property
-  // return `true` if they match, otherwise return `false`
-  comparePasswords(userPass) {
-    if (this.password === userPass) { return true; }
-    // else { return false; }
+    
+    // create a method on the User class called `checkPassword`
+    // this method should take in a string and compare it to the object's password property
+    // return `true` if they match, otherwise return `false`
+
+    checkPassword(options) {
+    return this.password === options.password;
   }
 }
 
@@ -33,6 +34,8 @@ const checkPassword = function comparePasswords(passwordToCompare) {
   // use `this` to access the object's `password` property.
   // do not modify this function's parameters
   // note that we use the `function` keyword and not `=>`
+  return this.password === passwordToCompare;
+  
 };
 
 // invoke `checkPassword` on `me` by explicitly setting the `this` context
