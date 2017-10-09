21e0c52b576758f39d647786dd9f82e7b69b2034
Alex Cassell <alex@simulism.net>
2017-10-09 10:18:44 -0400

    All tests pass

--- src/recursion.js
+++ src/recursion.js
@@ -17,14 +17,27 @@ const nFactorial = (n) => {
 /* Extra Credit */
 // Not working
 const checkMatchingLeaves = (obj) => {
-  const val = [];
-  (function rec(tmpObj) {
-    Object.keys(tmpObj).forEach((key) => {
-      if (typeof tmpObj[key] === 'object') return rec(obj[key]);
-      val.push(tmpObj[key]);
+  // return true if every property on `obj` is the same
+  // otherwise return false
+  // this works but it took a lot of help and I certainly could not do it again on my own
+  let val;
+  let match = true;
+  const check = (object) => {
+    Object.keys(object).forEach((key) => {
+      if (val === undefined && key instanceof Object !== true) {
+        val = object[key];
+        return undefined;
+      }
+      if ((object[key]) instanceof Object === true) return check(object[key]);
+      if ((object[key]) !== val) {
+        match = false;
+        return undefined;
+      }
+      return undefined;
     });
-  }(obj));
-  return val.every(x => val[0] === x);
+  };
+  check(obj);
+  return match;
 };
 
 /* eslint-enable no-unused-vars */
