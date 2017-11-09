ed64e34e520d028ba5cc251c4c2dc3ab9280c3da
Kwaku Dapaah <kwakudapaah@Kwakus-MBP.fios-router.home>
2017-11-09 18:41:50 -0500

    changes

--- src/this.js
+++ src/this.js
@@ -7,8 +7,16 @@
 class User {
   constructor(options) {
     // set a username and password property on the user object that is created
+    this.username = options.username;
+    this.password = options.password;
   }
   // create a method on the User class called `checkPassword`
+  checkPassword(str) {
+    if (str === this.password) {
+      return true;
+    }
+    return false;
+  }
   // this method should take in a string and compare it to the object's password property
   // return `true` if they match, otherwise return `false`
 }
@@ -27,6 +35,10 @@ const checkPassword = function comparePasswords(passwordToCompare) {
   // use `this` to access the object's `password` property.
   // do not modify this function's parameters
   // note that we use the `function` keyword and not `=>`
+  if(passwordToCompare === this.password) {
+    return true;
+  }
+  return false;
 };
 
 // invoke `checkPassword` on `me` by explicitly setting the `this` context
