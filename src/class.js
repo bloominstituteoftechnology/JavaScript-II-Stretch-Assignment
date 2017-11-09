be40d464f63d707311ba3111d28819249abde169
Jesse Hood <jessenoob@gmail.com>
2017-11-08 20:40:18 -0500

    solved class.js

--- src/class.js
+++ src/class.js
@@ -23,6 +23,37 @@
 
 /* eslint-disable no-undef */
 
+class User {
+  constructor(options) {
+    this.email = options.email;
+    this.password = options.password;
+  }
+  comparePasswords(password) {
+    return this.password === password;
+  }
+}
+
+class Animal {
+  constructor(options) {
+    this.age = options.age;
+  }
+
+  growOlder() {
+    return this.age++;
+  }
+}
+
+class Cat extends Animal {
+  constructor(options) {
+    super(options);
+    this.name = options.name;
+  }
+
+  meow() {
+    return `${this.name} meowed!`;
+  }
+}
+
 module.exports = {
   User,
   Cat,
