5b2a3b190cc5f86dff575186ca66a0ca8d48c2fd
fairlight-excalibur <lidgrt@gmail.com>
2017-11-08 19:21:24 -0600

    nearly done

--- src/recursion.js
+++ src/recursion.js
@@ -1,17 +1,37 @@
 // Complete the following functions.
 
 const nFibonacci = (n) => {
-  // fibonacci sequence: 1 2 3 5 8 13 ...
+  if (n === 1) {
+    return 1;
+  }
+  if (n === 2) {
+    return 2;
+  }
+  return (nFibonacci(n - 1) + nFibonacci(n - 2));
+  // fibonacci sequence: [0 1] 1 2 3 5 8 13 ...
   // return the nth number in the sequence
 };
 
 const nFactorial = (n) => {
+  if (n === 1) {
+    return 1;
+  }
+  return (n * nFactorial(n - 1));
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
 };
 
 /* Extra Credit */
 const checkMatchingLeaves = (obj) => {
+  const keyRing = obj.keys();
+  for (int i = 0; i < keyRing; i++ ){
+    if (typeof keyRing[i] === 'object') {
+      checkMatchingLeaves(keyRing[i]);
+    } else {
+      if(){}
+    }
+  }
+  return true;
   // return true if every property on `obj` is the same
   // otherwise return false
 };
