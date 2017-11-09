605222eb5b8e066ae328d5d75fe2caa02258e321
Roarka <designerexpert@gmail.com>
2017-11-08 23:04:21 -0500

    Finished Homework

--- src/recursion.js
+++ src/recursion.js
@@ -18,8 +18,23 @@ const nFactorial = (n) => {
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
-  const ObjVals = Object.values(obj);
-  return Object.is(ObjVals);
+  const combineObj = (...params) => { // Pass in an object to be combined into an array
+    let newArr = [];
+    Object.values(...params).forEach((objectValue) => { // Iterate over an array of the object's values
+      if (typeof objectValue === 'object' && objectValue !== null) { // for every objectValue that happens to be also a nested object and has values.
+        newArr = newArr.concat(combineObj(objectValue));
+        return; // exit loop
+      }
+      newArr.push(objectValue); // Add the values to the end of the newArr
+    });
+    return newArr; // Return array to be used in comparing the values function.
+  };
+
+  const objectToArray = combineObj(obj);
+
+  const returnVal = objectToArray.filter(testValue => objectToArray[0] !== testValue);
+
+  return returnVal.length === 0;
 };
 
 /* eslint-enable no-unused-vars */
