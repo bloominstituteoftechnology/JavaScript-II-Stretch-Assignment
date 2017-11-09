6278f697218da6cd3a38cccf1c402b95aa6e11d6
Jesse Hood <jessenoob@gmail.com>
2017-11-08 20:11:38 -0500

    solved part 1 of this.js

--- src/this.js
+++ src/this.js
@@ -7,10 +7,16 @@
 class User {
   constructor(options) {
     // set a username and password property on the user object that is created
+    this.username = options.username;
+    this.password = options.password;
   }
   // create a method on the User class called `checkPassword`
   // this method should take in a string and compare it to the object's password property
   // return `true` if they match, otherwise return `false`
+
+  checkPassword(password) {
+    return this.password === password;
+  }
 }
 
 const me = new User({
@@ -20,6 +26,7 @@ const me = new User({
 
 const result = me.checkPassword('correcthorsebatterystaple'); // should return `true`
 
+
 /* part 2 */
 
 const checkPassword = function comparePasswords(passwordToCompare) {
@@ -27,6 +34,7 @@ const checkPassword = function comparePasswords(passwordToCompare) {
   // use `this` to access the object's `password` property.
   // do not modify this function's parameters
   // note that we use the `function` keyword and not `=>`
+
 };
 
 // invoke `checkPassword` on `me` by explicitly setting the `this` context
