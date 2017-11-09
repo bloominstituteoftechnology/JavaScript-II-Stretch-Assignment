09359271f3059bcfff6e4786fec34d4230400bda
Pat Collins <padjo@mac.com>
2017-11-09 02:22:45 +0000

    recursion:Fibonacci working

--- src/recursion.js
+++ src/recursion.js
@@ -1,8 +1,15 @@
 // Complete the following functions.
 
+// https://www.sitepoint.com/recursion-functional-javascript/
+
+// see https://medium.com/developers-writing/fibonacci-sequence-algorithm-in-javascript-b253dc7e320e
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  // check for base and return
+  if (n <= 1) return 1;
+  // otherwise we know Fibonacci(n) is made of up Fib(n-2) + Fib(n-1) which is recursive
+  return nFibonacci(n - 1) + nFibonacci(n - 2);
 };
 
 const nFactorial = (n) => {
