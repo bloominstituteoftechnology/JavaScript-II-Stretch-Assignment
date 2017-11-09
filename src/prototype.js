e6c59d70b24b269f224e4e46ccdaec5b160b0db1
Kwaku Dapaah <kwakudapaah@Kwakus-MBP.fios-router.home>
2017-11-09 17:58:07 -0500

    changes

--- src/prototype.js
+++ src/prototype.js
@@ -51,6 +51,42 @@
 
 /* eslint-disable no-undef */
 
+
+// GameObject constructor function.
+function GameObject(options) {
+  this.createdAt = options.createdAt;
+  this.dimensions = options.dimensions;
+}
+
+GameObject.prototype.destroy = () => {
+  return 'Game object was removed from the game.';
+};
+
+
+// NPC constructor function which will inherit from GameObject.
+function NPC(options) {
+  GameObject.call(this, options);
+  this.hp = options.hp;
+  this.name = options.name;
+}
+NPC.prototype = Object.create(GameObject.prototype);
+NPC.prototype.takeDamage = function () {
+  return `${this.name} took damage.`;
+};
+
+// Humanoid constructor fuction will inherit from NPC and GameObject
+function Humanoid(options) {
+  NPC.call(this, options);
+  this.faction = options.faction;
+  this.weapons = options.weapons;
+  this.language = options.language;
+}
+Humanoid.prototype = Object.create(NPC.prototype);
+Humanoid.prototype.greet = function () {
+  return `${this.name} offers a greeting in ${this.language}.`;
+};
+
+
 module.exports = {
   GameObject,
   NPC,
