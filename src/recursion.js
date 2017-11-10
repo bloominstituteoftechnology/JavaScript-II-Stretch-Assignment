52090d437fdbd0574ee95597d7bd681ea70b229d
Franz <franzjfernando@hotmail.com>
2017-11-09 18:31:37 -0800

    completed this.js still needs refactoring

--- src/recursion.js
+++ src/recursion.js
@@ -12,26 +12,53 @@ const nFibonacci = (n) => {
   // const result = (n < 2) ? n : nFibonacci(n - 1) + nFibonacci(n - 2);
   // return result;
 };
+nFibonacci(10);
+
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
-  if (n === 0) {
+  if (n <= 0) {
     return 1;
   }
   return n * nFactorial(n - 1);
 };
+nFactorial(19);
 
 /* Extra Credit */
+
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
-    objArray.forEach(function(leaf, checked) {
-      if (leaf === objArray[checked]) {
-        return true;
+// * **********************attempt **********************/
+//   obj.forEach(leaf, checked) => {
+//     if (leaf === obj[checked]) {
+//       return true;
+//     }
+//     return false;
+//   });
+// };
+// ***********************In class solution */
+  let tree;
+  let val;
+  let flag = true;
+  const checkLeaves = () => {
+    Object.keys(tree).forEach((key) => {
+      if (val === undefined && typeof key !== 'object') {
+        val = tree[key];
+        return undefined;
       }
-    
-  }, this);
+      if (typeof tree[key] === 'object') return checkLeaves(tree[key]);
+      if (tree[key] !== val) {
+        flag = false;
+        return undefined;
+      }
+      return undefined;
+    });
+  };
+  checkLeaves(obj);
+  return flag;
 };
+checkMatchingLeaves();
 
 /* eslint-enable no-unused-vars */
 
