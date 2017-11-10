2b5b85970dfb1cd0094b59d0dd37ca108a93face
JasonCorp84 <csababalogh84@gmail.com>
2017-11-09 16:41:10 -0800

    almost done

--- practice.js
+++ practice.js
@@ -1,20 +1,3 @@
-let obj = {
-  mama: 'tata',
-  baba: 'lol'
-}
-
-function add () {
-  console.log(this.mama);
-}
-
-console.log(add());
-
-const checkMatchingLeaves = (obj) => {
-  // return true if every property on `obj` is the same
-  // otherwise return false
-  let = keys = Object.obj;
-  console.log(keys);
-};
 
 const tree2 = {
   x: 1,
@@ -34,4 +17,27 @@ const tree2 = {
   },
   r: 1,
 };
+const checkMatchingLeaves = (obj) => {
+  // return true if every property on `obj` is the same
+  // otherwise return false
+  let val;
+  let flag = true;
+  const checkLeaves = (tree) => {
+    Object.keys(tree).forEach((key) => {
+      if(val === undefined && typeof key !== 'object') {
+        val = tree[key];
+        return undefined;
+      }
+      if (typeof tree[key] === 'object') return checkLeaves(tree[key]);
+      if (tree[key] !== value) {
+        flag = false;
+        return undefined;
+      }
+      return undefined;
+    });
+  }
+  checkLeaves(obj);
+  return flag;
+};
+
 checkMatchingLeaves (tree2);
--- src/recursion.js
+++ src/recursion.js
@@ -2,11 +2,9 @@
 
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 2 3 5 8 13 ...
-  //if (n <= 1) return 1;
-  //return nFibonacci(n - 1) + nFibonacci(n - 2);*/
-  const result = (n < 2) ? n : nFibonacci(n - 1) + nFibonacci(n - 2);
-  return result;
   // return the nth number in the sequence
+  if (n <= 1) return 1;
+  return nFibonacci(n - 1) + nFibonacci(n - 2);
 };
 
 const nFactorial = (n) => {
@@ -20,6 +18,24 @@ const nFactorial = (n) => {
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  let val;
+  let flag = true;
+  const checkLeaves = (tree) => {
+    Object.keys(tree).forEach((key) => {
+      if (val === undefined && typeof key !== 'object') {
+        val = tree[key];
+        return undefined;
+      }
+      if (typeof tree[key] === 'object') return checkLeaves(tree[key]);
+      if (tree[key] !== val) {
+        flag = false;
+        return undefined;
+      }
+      return undefined;
+    });
+  };
+  checkLeaves(obj);
+  return flag;
 };
 
 /* eslint-enable no-unused-vars */
