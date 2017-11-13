22d1daed1dc91e1ae0ba125b1d1a59e351e3ff1a
Xiaoping Li <narci2008@gmail.com>
2017-11-12 22:10:53 -0800

    modified homework

--- src/prototype.js
+++ src/prototype.js
@@ -48,6 +48,41 @@
   hamsterHuey.takeDamage(); // returns 'Hamster Huey took damage.'
   hamsterHuey.destroy(); // returns 'Game object was removed from the game.'
 */
+class GameObject {
+  constructor(options) {
+    this.createdAt = options.createdAt;
+    this.dimensions = options.dimensions;
+  }
+
+  destroy() {
+    return 'Game object was removed from the game.';
+  }
+}
+
+class NPC extends GameObject {
+  constructor(options) {
+    super(options);
+    this.hp = options.hp;
+    this.name = options.name;
+  }
+
+  takeDamage() {
+    return `${this.name} took damage.`;
+  }
+}
+
+class Humanoid extends NPC {
+  constructor(options) {
+    super(options);
+    this.faction = options.faction;
+    this.weapons = options.weapons;
+    this.language = options.language;
+  }
+
+  greet() {
+    return `${this.name} offers a greeting in ${this.language}.`;
+  }
+}
 
 /* eslint-disable no-undef */
 
--- src/recursion.js
+++ src/recursion.js
@@ -3,17 +3,45 @@
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  if (n <= 0) return;
+  if (n <= 1) return 1;
+  if (n === 2) return 2;
+  return nFibonacci(n - 1) + nFibonacci(n - 2);
 };
 
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+  if (n < 0) return;
+  if (n === 0) return 1;
+  return n * nFactorial(n - 1);
 };
 
 /* Extra Credit */
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  const flatten = (tree) => {
+    let flattenArray = [];
+    const treeKeys = Object.keys(tree);
+    treeKeys.forEach((key) => {
+      if (typeof tree[key] !== 'object') {
+        flattenArray = flattenArray.concat(tree[key]);
+      } else {
+        flattenArray = flattenArray.concat(flatten(tree[key]));
+      }
+    });
+    return flattenArray;
+  };
+  let flag = true;
+  const keyValues = flatten(obj);
+  const value = keyValues[0];
+  keyValues.forEach((elements) => {
+    if (elements !== value) {
+      flag = false;
+    }
+  });
+  return flag;
 };
 
 /* eslint-enable no-unused-vars */
