e7070fbe8f9d43dc1dacb51b6284a6f7e5fdfcfe
John C <johnjohnx4@gmail.com>
2017-12-07 18:50:37 -0500

    deleted comment blocks

--- src/recursion.js
+++ src/recursion.js
@@ -1,8 +1,4 @@
-// Complete the following functions.
-
 const nFibonacci = (n) => {
-  // fibonacci sequence: 1 2 3 5 8 13 ...
-  // return the nth number in the sequence
   if (n === 0) return 0;
   else if (n === 1) return 1;
   else if (n === 2) return 2;
@@ -10,16 +6,12 @@ const nFibonacci = (n) => {
 };
 
 const nFactorial = (n) => {
-  // factorial example: !5 = 5 * 4 * 3 * 2 * 1
-  // return the factorial of `n`
   if (n === 0) return 1;
   return n * nFactorial(n - 1);
 };
 
 /* Extra Credit */
 const checkMatchingLeaves = (obj) => {
-  // return true if every property on `obj` is the same
-  // otherwise return false
   let val;
   let flag = true;
   const checkLeaves = (tree) => {
