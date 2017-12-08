4534ae8202d916d2a3ca0fbd83bd732df9e01265
iradsanchez <ira.d.sanchez@gmail.com>
2017-12-08 12:33:36 -0700

    Ira Sanchez Dec 7 Homework

--- src/recursion.js
+++ src/recursion.js
@@ -3,20 +3,28 @@
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  if (n <= 1) return 1;
+  return nFibonacci(n - 1) + nFibonacci(n - 2);
 };
 
 const nFactorial = (n) => {
-  // factorial example: !5 = 5 * 4 * 3 * 2 * 1
-  // return the factorial of `n`
+  if (n === 0) return 1;
+  return n * nFactorial(n - 1);
 };
 
-/* Extra Credit */
 const checkMatchingLeaves = (obj) => {
-  // return true if every property on `obj` is the same
-  // otherwise return false
+  // initialize some value...
+  // initialize some flag.. boolean.
+  // initialize some helper function and pass obj... leaf checker recursive function
+    // check all the keys = this is a hint......for loop of some kind
+    // if a key is an object && initial value is undef
+      // assign value
+    // if a value is an object ==> recurse
+    // if a value is found, and it doesn't match our initial value
+    // trip our flag to false
+    // return;
+  // return true/false
 };
-
-/* eslint-enable no-unused-vars */
 
 module.exports = {
   nFibonacci,
