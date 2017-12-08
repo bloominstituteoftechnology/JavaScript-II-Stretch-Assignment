7e5c05f6e901667524c536388a8b7c9b81f911d8
barrettjviatorlambdaschool <31706127+barrettjviatorlambdaschool@users.noreply.github.com>
2017-12-08 08:22:40 -0600

    did lots of effort

--- src/prototype.js
+++ src/prototype.js
@@ -25,29 +25,51 @@
   Inheritance chain: Humanoid -> NPC -> GameObject
   Instances of Humanoid should have all of the same properties as NPC and GameObject.
   Instances of NPC should have all of the same properties as GameObject.
-
-  Example:
-
-  const hamsterHuey = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    hp: 5,
-    name: 'Hamster Huey',
-    faction: 'Gooey Kablooie',
-    weapons: [
-      'bubblegum',
-    ],
-    language: 'Hamsterish',
-  });
-
-  hamsterHuey.greet(); // returns 'Hamster Huey offers a greeting in Hamsterish'
-  hamsterHuey.takeDamage(); // returns 'Hamster Huey took damage.'
-  hamsterHuey.destroy(); // returns 'Game object was removed from the game.'
 */
+function GameObject(properties) {
+  this.createdAt = properties.createdAt;
+  this.dimensions = properties.dimensions;
+}
+GameObject.prototype.destroy = function _() {
+  return 'Game object was removed from the game.';
+};
+function NPC(stats) {
+  GameObject.call(this, stats);
+  this.hp = stats.hp;
+  this.name = stats.name;
+}
+function Humanoid(characteristics) {
+  NPC.call(this, characteristics);
+  this.faction = characteristics.faction;
+  this.weapons = characteristics.weapons;
+  this.language = characteristics.language;
+}
+NPC.prototype = Object.create(GameObject.prototype);
+NPC.prototype.takeDamage = function _() {
+  return `${this.name} took damage.`;
+};
+
+Humanoid.prototype = Object.create(NPC.prototype);
+Humanoid.prototype.greet = function _() {
+  return `${this.name} offers a greeting in ${this.language}.`;
+};
+const corgiYuki = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 3,
+    width: 1.5,
+    height: 1.5,
+  },
+  hp: 567,
+  name: 'Yuki Sempai',
+  faction: 'Snow Princess Caravan',
+  weapons: ['Kendo Stick', 'Surprise Wand'],
+  language: 'Welsh',
+});
+
+// function greet() { return greet('<object name> offers a greeting in <object language>'); }
+// function takeDamage() { return takeDamage('took damage'); }
+// function destroy() { return destroy('Game object was removed from the game'); }
 
 /* eslint-disable no-undef */
 
--- src/this.js
+++ src/this.js
@@ -1,39 +1,32 @@
-// Follow the instructions and fill in the blank sections.
-// There are no tests for this file.
-// To verify your code works you can run this file using `node this.js` while in the `/src` folder
-
-/* part 1 */
-
 class User {
   constructor(options) {
-    // set a username and password property on the user object that is created
+    this.username = options.username;
+    this.password = options.password;
   }
-  // create a method on the User class called `checkPassword`
-  // this method should take in a string and compare it to the object's password property
-  // return `true` if they match, otherwise return `false`
+  checkPassword(str) {
+    return str === this.password;
+  }
 }
 
-const me = new User({
-  username: 'LambdaSchool',
-  password: 'correcthorsebatterystaple',
-});
-
-const result = me.checkPassword('correcthorsebatterystaple'); // should return `true`
-
-/* part 2 */
-
-const checkPassword = function comparePasswords(passwordToCompare) {
-  // recreate the `checkPassword` method that you made on the `User` class
-  // use `this` to access the object's `password` property.
-  // do not modify this function's parameters
-  // note that we use the `function` keyword and not `=>`
+const me = {
+  username: 'barrettjviator',
+  password: 'itsasecret',
 };
 
-// invoke `checkPassword` on `me` by explicitly setting the `this` context
-// use .call, .apply, and .bind
+const barrett = new User(me); 
+
+// first test
+const result = barrett.checkPassword('ittsasecret'); 
+console.log(result);
 
 // .call
+const result2 = barrett.checkPassword.call(barrett, 'itsasecret');
+console.log(result2);
 
 // .apply
+const result3 = barrett.checkPassword.apply('itsasecret');
+console.log(result3);
 
 // .bind
+const result4 = barrett.checkPassword.bind(barrett, 'itsasecret');
+console.log(result4);
