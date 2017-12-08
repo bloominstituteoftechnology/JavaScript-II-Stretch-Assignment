bdd73707e2fc7293fd87768cf1e045064206c0fb
Guelmis Cortina <cguelmis@yahoo.com>
2017-12-08 10:45:39 -0500

    done

--- src/this.js
+++ src/this.js
@@ -7,10 +7,16 @@
 class User {
   constructor(options) {
     // set a username and password property on the user object that is created
+    this.username = options.username;
+    this.password = options.password;
   }
   // create a method on the User class called `checkPassword`
   // this method should take in a string and compare it to the object's password property
   // return `true` if they match, otherwise return `false`
+  checkPassword(potPassword) {
+    if (potPassword === this.password) return true;
+    return false;
+  }
 }
 
 const me = new User({
@@ -19,7 +25,7 @@ const me = new User({
 });
 
 const result = me.checkPassword('correcthorsebatterystaple'); // should return `true`
-
+console.log(result);
 /* part 2 */
 
 const checkPassword = function comparePasswords(passwordToCompare) {
@@ -27,6 +33,8 @@ const checkPassword = function comparePasswords(passwordToCompare) {
   // use `this` to access the object's `password` property.
   // do not modify this function's parameters
   // note that we use the `function` keyword and not `=>`
+  if (passwordToCompare === this.password) return true;
+  return false;
 };
 
 // invoke `checkPassword` on `me` by explicitly setting the `this` context
@@ -37,3 +45,8 @@ const checkPassword = function comparePasswords(passwordToCompare) {
 // .apply
 
 // .bind
+
+console.log(checkPassword.call(me, 'correcthorsebatterystaple'));
+console.log(checkPassword.apply(me, ['correcthorsebatterystaple']));
+const checkPass = checkPassword.bind(me);
+console.log(checkPass('correcthorsebatterystaple'));
