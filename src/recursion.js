3dfdd345616d2c38ad80f47d0615fbe0424982ef
Jessica Ju-Yuan Liu <Airistotle@Airistotle.local>
2017-12-08 10:50:03 -0500

    JavaScript-II

--- src/recursion.js
+++ src/recursion.js
@@ -1,19 +1,34 @@
 // Complete the following functions.
 
 const nFibonacci = (n) => {
-  // fibonacci sequence: 1 2 3 5 8 13 ...
-  // return the nth number in the sequence
+  if (n <= 1) return 1;
+  return nFibonacci(n - 1) + nFibonacci(n - 2);
 };
 
 const nFactorial = (n) => {
-  // factorial example: !5 = 5 * 4 * 3 * 2 * 1
-  // return the factorial of `n`
+  if (n === 0) return 1;
+  return n * nFactorial(n - 1);
 };
 
 /* Extra Credit */
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+
+  const index = 0;
+  const objArray = Object.values(obj);
+
+  const testFunction = (arr, i) => {
+    while (i < arr.length - 1) {
+      if (arr[i] !== arr[++i]) {
+        return false;
+      }
+      return testFunction(arr, i++);
+    }
+    return true;
+  };
+
+  return testFunction(objArray, index);
 };
 
 /* eslint-enable no-unused-vars */
