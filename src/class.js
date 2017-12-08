08dbb97dad83a002a652de69e3f22dbb26291a0e
Camila Daniels <camila.daniels926@gmail.com>
2017-12-08 09:56:17 -0600

    Camila Daniels camila.daniels926@gmail.com

--- src/class.js
+++ src/class.js
@@ -8,6 +8,16 @@
 // Return true if the potential password matches the `password` property.  Otherwise return false.
 
 // code here
+class User {
+  constructor(options) {
+    this.email = options.email;
+    this.password = options.password;
+  }
+  comparePasswords(compareTo) {
+    if (this.password === compareTo) return true;
+    return false;
+  }
+}
 
 // Part 2
 // Create a class called `Animal` and a class called `Cat` using ES6 classes.
@@ -22,6 +32,25 @@
 // code here
 
 /* eslint-disable no-undef */
+class Animal {
+  constructor(options) {
+    this.age = options.age;
+  }
+  growOlder() {
+    this.age++;
+    return this.age;
+  }
+}
+
+class Cat extends Animal {
+  constructor(options) {
+    super(options);
+    this.name = options.name;
+  }
+  meow() {
+    return `${this.name} meowed!`;
+  }
+}
 
 module.exports = {
   User,
--- src/recursion.js
+++ src/recursion.js
@@ -1,19 +1,39 @@
 // Complete the following functions.
 
+const each = (elements, cb) => {
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
+};
+
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  // return n + nFibonacci()
+  if (n <= 1) return 1;
+  return nFibonacci(n - 1) + nFibonacci(n - 2);
 };
 
-const nFactorial = (n) => {
-  // factorial example: !5 = 5 * 4 * 3 * 2 * 1
-  // return the factorial of `n`
-};
-
+// console.log(nFibonacci(6));
 /* Extra Credit */
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  // initialize  a variable that you can use to find the value at each node
+  let value;
+  Object.keys(obj).forEach((key) => {
+    if (typeof key === 'object') {
+      checkMatchingLeaves(key);
+    } else if (typeof key !== 'object' && value === undefined) {
+      value = key;
+    } else if (value !== undefined && value !== key && typeof key !== 'object') return false;
+    return true;
+  });
+};
+
+const nFactorial = (num) => {
+  if (num === 0) return 1;
+  return num * nFactorial(num - 1);
 };
 
 /* eslint-enable no-unused-vars */
