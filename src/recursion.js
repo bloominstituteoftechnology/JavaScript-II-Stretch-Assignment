a0b7b4fda07b75a586f34bc48778768d628a6158
Aaron <aarondburk@gmail.com>
2018-01-10 17:56:45 -0600

    committing changes before new pull

--- src/recursion.js
+++ src/recursion.js
@@ -3,12 +3,16 @@
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  if (n <= 1) return 1;
+  return (n - 1) + (n - 2);
 };
 
 const nFactorial = (n) => {
+  if (n <= 1) return 1;
+  return n * nFactorial(n - 1);
+};
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
-};
 
 /* Extra Credit */
 const checkMatchingLeaves = (obj) => {
