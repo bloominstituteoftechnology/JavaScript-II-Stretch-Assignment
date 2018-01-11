651ab46bf05b379a43c8433a33e59a9445737505
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-01-10 19:52:36 -0500

    Completed nFibonacci and nFactorial

--- src/recursion.js
+++ src/recursion.js
@@ -3,11 +3,15 @@
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  if (n <= 2) return 1;
+  return nFibonacci(n - 2) + nFibonacci(n - 1);
 };
 
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+  if (n <= 1) return 1;
+  return n * nFactorial(n - 1);
 };
 
 /* Extra Credit */
@@ -15,6 +19,11 @@ const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
 };
+
+// check first object value
+// compare first object value with every other object value
+// if any are different, return false
+// if all are equal, return true;
 
 /* eslint-enable no-unused-vars */
 
