0b591472fbff1419c6b1b3edc1f54e9da40c4531
Nikhil Kamineni <nikhil@nk-mbp.local>
2018-01-11 18:53:24 -0500

    Finished part 1 of class.js

--- src/class.js
+++ src/class.js
@@ -8,6 +8,16 @@
 // Return true if the potential password matches the `password` property.  Otherwise return false.
 
 // code here
+class User {
+  constructor(options) {
+    this.email = options.email;
+    this.password = options.password;
+  }
+  comparePasswords(enteredPassword) {
+    if (this.password === enteredPassword) return true;
+    return false;
+  }
+}
 
 // Part 2
 // Create a class called `Animal` and a class called `Cat` using ES6 classes.
