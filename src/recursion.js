b3eb1a80d3a85a262c833279d68bfd7ee719b152
Lo Saephan <teamsky916@gmail.com>
2018-01-10 16:11:47 -0800

    js-2 partial

--- src/recursion.js
+++ src/recursion.js
@@ -3,11 +3,15 @@
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+
 };
 
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+  if (n <= 0) return 1;
+  let sum = nFactorial(n - 1);
+  return n * sum;
 };
 
 /* Extra Credit */
