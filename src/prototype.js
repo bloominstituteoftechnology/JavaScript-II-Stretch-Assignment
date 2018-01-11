725f809ea51f670fe71567db086514699db690e0
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-01-11 16:00:02 -0500

    Fixed an error in prototype.js

--- src/prototype.js
+++ src/prototype.js
@@ -62,25 +62,23 @@ function NPC(options) {
   this.hp = options.hp;
   this.name = options.name;
 }
-NPC.prototype = Object.create(GameObject.prototype);
 
+NPC.prototype = Object.create(GameObject.prototype);
 NPC.prototype.takeDamage = function () {
   return `${this.name} took damage.`;
 };
 
-function Humanoid(humanoidOptions) {
-  NPC.call(this, humanoidOptions);
-  this.faction = humanoidOptions.faction;
-  this.weapons = humanoidOptions.weapons;
-  this.language = humanoidOptions.language;
+function Humanoid(options) {
+  NPC.call(this, options);
+  this.faction = options.faction;
+  this.weapons = options.weapons;
+  this.language = options.language;
 }
-Humanoid.prototype = Object.create(GameObject.prototype);
-Humanoid.prototype = Object.create(NPC.prototype);
 
+Humanoid.prototype = Object.create(NPC.prototype);
 Humanoid.prototype.greet = function () {
   return `${this.name} offers a greeting in ${this.language}.`;
 };
-
 /* eslint-disable no-undef */
 
 module.exports = {
