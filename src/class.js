235a4ad8a1e2402e12db0fd342f9bf9144e6070a
rcs784 <rcstinson@hotmail.com>
2018-01-11 18:01:09 -0500

    complete hw 1/11

--- src/class.js
+++ src/class.js
@@ -7,6 +7,17 @@
 // for a potential password that will be compared to the `password` property.
 // Return true if the potential password matches the `password` property.  Otherwise return false.
 
+class User {
+  constructor(options) {
+    this.email = options.email;
+    this.password = options.password;
+  }
+
+  comparePasswords(potentialPassword) {
+    return this.password === potentialPassword;
+  }
+}
+
 // code here
 
 // Part 2
@@ -20,6 +31,27 @@
 // property set on the Cat instance.
 
 // code here
+
+class Animal {
+  constructor(options) {
+    this.age = options.age;
+  }
+
+  growOlder() {
+    return ++this.age;
+  }
+}
+
+class Cat extends Animal {
+  constructor(options) {
+    super(options);
+    this.name = options.name;
+  }
+
+  meow() {
+    return `${this.name} meowed!`;
+  }
+}
 
 /* eslint-disable no-undef */
 
