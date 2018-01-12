0536753482b49e107fe47784efeb93e04ae93a55
Michael Marshalkovich <mikemarshalman@gmail.com>
2018-01-12 00:38:37 -0600

    Initial commit

--- src/class.js
+++ src/class.js
@@ -8,7 +8,16 @@
 // Return true if the potential password matches the `password` property.  Otherwise return false.
 
 // code here
-
+class User {
+  constructor(options) {
+    this.email = options.email;
+    this.password = options.password;
+    // this.comparePassword = arg => arg === this.password;
+  }
+  comparePasswords(arg) {
+    return arg === this.password;
+  }
+}
 // Part 2
 // Create a class called `Animal` and a class called `Cat` using ES6 classes.
 // `Cat` should extend the `Animal` class.
@@ -20,7 +29,24 @@
 // property set on the Cat instance.
 
 // code here
-
+class Animal {
+  constructor(options) {
+    this.age = options.age;
+  }
+  growOlder() {
+    this.age += 1;
+    return this.age;
+  }
+}
+class Cat extends Animal {
+  constructor(options) {
+    super(options);
+    this.name = options.name;
+  }
+  meow() {
+    return `${this.name} meowed!`;
+  }
+}
 /* eslint-disable no-undef */
 
 module.exports = {
--- src/recursion.js
+++ src/recursion.js
@@ -3,19 +3,37 @@
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  if (n <= 1) {
+    return 1;
+  }
+  return nFibonacci(n - 1) + nFibonacci(n - 2);
 };
 
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+  if (n === 0) {
+    return 1;
+  }
+  return n * nFactorial(n - 1);
 };
 
 /* Extra Credit */
+let singleProp = null;
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  Object.keys(obj).forEach((key) => {
+    if (typeof obj[key] === 'object') {
+      checkMatchingLeaves(obj[key]);
+    } else if (singleProp === null) {
+      singleProp = obj[key];
+    } else if (singleProp !== obj[key]) {
+      singleProp = 99;
+    }
+  });
+  return (singleProp !== 99);
 };
-
 /* eslint-enable no-unused-vars */
 
 module.exports = {
