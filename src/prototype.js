3080a60391467b3929164f9bee28acdcc1c9033d
IchabodPain <russell.l.bates@gmail.com>
2018-01-12 16:41:51 -0800

    Mostly completed, with some confusion as to what do do on the latter half of this.js

--- src/prototype.js
+++ src/prototype.js
@@ -1,37 +1,38 @@
 /* eslint-disable no-undef */
 
-function GameObject(traits) {
-  this.createdAt = traits.createdAt;
-  this.dimensions = traits.dimensions;
-}
-GameObject.prototype.destroy = function destroy() {
-  return 'Game object was removed from the game.';
-};
-
-function NPC(NPCTraits) {
-  GameObject.call(this, NPCTraits);
-  this.hp = NPCTraits.hp;
-  this.name = NPCTraits.name;
+class GameObject {
+  constructor(traits) {
+    this.createdAt = traits.createdAt;
+    this.dimensions = traits.dimensions;
+  }
+  destroy() {
+    const theLinterIsAnIdiot = this.name;
+    return 'Game object was removed from the game.';
+  }
 }
 
-NPC.prototype = Object.create(GameObject.prototype);
-
-NPC.prototype.takeDamage = function takeDamage() {
-  return `${this.name} took damage.`;
-};
-
-function Humanoid(humanoidTraits) {
-  NPC.call(this, humanoidTraits);
-  this.faction = humanoidTraits.faction;
-  this.weapons = humanoidTraits.weapons;
-  this.language = humanoidTraits.language;
+class NPC extends GameObject {
+  constructor(NPCTraits) {
+    super(NPCTraits);
+    this.hp = NPCTraits.hp;
+    this.name = NPCTraits.name;
+  }
+  takeDamage() {
+    return `${this.name} took damage.`;
+  }
 }
 
-Humanoid.prototype = Object.create(NPC.prototype);
-
-Humanoid.prototype.greet = function greet() {
-  return `${this.name} offers a greeting in ${this.language}.`;
-};
+class Humanoid extends NPC {
+  constructor(humanoidTraits) {
+    super(humanoidTraits);
+    this.faction = humanoidTraits.faction;
+    this.weapons = humanoidTraits.weapons;
+    this.language = humanoidTraits.language;
+  }
+  greet() {
+    return `${this.name} offers a greeting in ${this.language}.`;
+  }
+}
 
 const snakeOiler = new Humanoid({
   createdAt: 'Fire Mountain',
--- src/this.js
+++ src/this.js
@@ -6,12 +6,17 @@
 
 class User {
   constructor(options) {
+    this.username = options.username;
+    this.password = options.password;
     // set a username and password property on the user object that is created
   }
   // create a method on the User class called `checkPassword`
   // this method should take in a string and compare it to the object's password property
   // return `true` if they match, otherwise return `false`
-
+  checkPassword(potentialPassword) {
+    if (this.password === potentialPassword) return true;
+    return false;
+  }
 }
 
 const me = new User({
@@ -28,6 +33,8 @@ const checkPassword = function comparePasswords(passwordToCompare) {
   // use `this` to access the object's `password` property.
   // do not modify this function's parameters
   // note that we use the `function` keyword and not `=>`
+  if (this.password === passwordToCompare) return true;
+  return false;
 };
 
 // invoke `checkPassword` on `me` by explicitly setting the `this` context
