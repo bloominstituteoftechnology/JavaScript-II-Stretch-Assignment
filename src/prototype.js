65ccbf572f4ee42dfb1497c31632bcacdfd5bc83
Igor Yermak <igor.yermak@gmail.com>
2018-01-14 16:44:16 -0500

    Prototypes, Recursion and This

--- src/prototype.js
+++ src/prototype.js
@@ -1,55 +1,49 @@
-/*
-  Object oriented design is commonly used in video games.  For this part of the assignment
-  you will be implementing several classes with their correct inheritance heirarchy.
+/* eslint-disable */
 
-  In this file you will be creating three classes:
-  GameObject
-    createdAt
-    dimensions
-    destroy() // prototype method -> returns the string 'Game object was removed from the game.'
+function GameObject(options) {
+  this.createdAt = options.createdAt;
+  this.dimensions = options.dimensions;
+}
+GameObject.prototype.destroy = function destroy() {
+  return ('${this.name}was removed from the game');
+};
 
-  NPC
-    hp
-    name
-    takeDamage() // prototype method -> returns the string '<object name> took damage.'
-    // should inherit destroy() from GameObject's prototype
+function NPC(npcOptions) {
+  GameObject.call(this, npcOptions);
+  this.hp = npcOptions.hp;
+  this.name = npcOptions.name;
+};
+NPC.prototype = Object.create(GameObject.prototype);
+NPC.prototype.takedamage = function takeDamage() {
+  return '${this.name}took damage.';
+};
 
-  Humanoid
-    faction
-    weapons
-    language
-    greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
-    // should inherit destroy() from GameObject through NPC
-    // should inherit takeDamage() from NPC
+function Humanoid(humOptions) {
+  NPC.call(this, humOptions);
+  this.faction = humOptions.faction;
+  this.weapons = humOptions.weapons;
+  this.language = humOptions.language;
+}
+Humanoid.prototype.greet = function greet() {
+  return '${this.name}offers a greeting in`${this.language}';
+}
 
-  Inheritance chain: Humanoid -> NPC -> GameObject
-  Instances of Humanoid should have all of the same properties as NPC and GameObject.
-  Instances of NPC should have all of the same properties as GameObject.
+const Bill = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1,
+  },
+  hp: 5,
+  name: 'Hamster Huey',
+  faction: 'Gooey Kablooie',
+  weapons: [
+    'bubblegum',
+  ],
+  language: 'Hamsterish',
 
-  Example:
-
-  const hamsterHuey = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    hp: 5,
-    name: 'Hamster Huey',
-    faction: 'Gooey Kablooie',
-    weapons: [
-      'bubblegum',
-    ],
-    language: 'Hamsterish',
-  });
-
-  hamsterHuey.greet(); // returns 'Hamster Huey offers a greeting in Hamsterish'
-  hamsterHuey.takeDamage(); // returns 'Hamster Huey took damage.'
-  hamsterHuey.destroy(); // returns 'Game object was removed from the game.'
-*/
-
-/* eslint-disable no-undef */
+});
 
 module.exports = {
   GameObject,
--- src/recursion.js
+++ src/recursion.js
@@ -1,25 +1,25 @@
 // Complete the following functions.
 
 const nFibonacci = (n) => {
-  // fibonacci sequence: 1 2 3 5 8 13 ...
-  // return the nth number in the sequence
+  if (n <= 2) return 1;
+  return nFibonacci(n - 1) + nFibonacci(n - 2);
 };
 
+// fibonacci sequence: 1 2 3 5 8 13 ...
+// return the nth number in the sequence
+
+
 const nFactorial = (n) => {
+  if (n <= 1) return 1;
+  return n * nFactorial(n - 1);
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
 };
 
-/* Extra Credit */
-const checkMatchingLeaves = (obj) => {
-  // return true if every property on `obj` is the same
-  // otherwise return false
-};
 
 /* eslint-enable no-unused-vars */
 
 module.exports = {
   nFibonacci,
   nFactorial,
-  checkMatchingLeaves,
 };
