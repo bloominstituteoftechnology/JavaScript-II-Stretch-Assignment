0d5e55e9749adfa9629e197adcb96c1bfa0fd78f
Res Yeg <shaqk702@gmail.com>
2018-01-15 08:50:49 -0800

    completed recursion

--- src/recursion.js
+++ src/recursion.js
@@ -3,17 +3,35 @@
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  if (n <= 2) return 1;
+  return nFibonacci(n - 2) + nFibonacci(n - 1);
 };
 
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+  if (n === 1) {
+    return 1;
+  }
+  return n * nFactorial(n - 1);
 };
 
 /* Extra Credit */
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  const leaves = [];
+  const findLeaves = (object) => {
+    Object.values(object).forEach((value) => {
+      if (value === Object(value)) {
+        findLeaves(value);
+      } else {
+        leaves.push(value);
+      }
+    });
+  };
+  findLeaves(obj);
+  return leaves.every(x => x === leaves[0]);
 };
 
 /* eslint-enable no-unused-vars */
