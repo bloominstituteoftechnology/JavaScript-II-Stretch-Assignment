5828722eb06f5449b10ea1f7f255474845cd22c1
Andrew Jarrett <ahrjarrett@gmail.com>
2018-01-18 18:15:14 -0700

    solve recursion problem

--- src/recursion.js
+++ src/recursion.js
@@ -10,29 +10,37 @@ const nFactorial = n => (
     ? 1
     : n * nFactorial(n - 1));
 
-// const hasSameValue = () => {}
 
 /* Extra Credit */
+
+// return true if every property on `obj` is the same
+// otherwise return false
+
+/* pseudocode:
+ * 1. traverse tree until we find a leaf
+ * 2. at first leaf: if checkVal is undefined, checkVal = leafVal
+ * 3. else if checkVal === leafVal return false
+ * 4. else return true
+ */
 const checkMatchingLeaves = (obj) => {
-  // return true if every property on `obj` is the same
-  // otherwise return false
+  let result = [];
 
-  let checkVal;
-
-  if (typeof obj === 'object') {
-    // return Object.keys(obj).forEach()
+  function _helper(obj) {
+    Object.keys(obj).forEach(key => {
+    if (typeof obj[key] === 'object') {
+      _helper(obj[key]);
+    }
+    else result.push(obj[key]);
+    });
   }
 
-  /* pseudocode:
-   * 1. traverse tree until we find a leaf
-   * 2. at first leaf: if checkVal is undefined, checkVal = leafVal
-   * 3. else if checkVal === leafVal return false
-   * 4. else return true
-   */
+  _helper(obj);
 
-
-  // Hint? Not sure if this is going to help...
-  // Object.prototype.toString.call(obj) === '[object Object]'
+  return !!result.reduce(function(a, b) {
+    return (a === b)
+      ? a
+      : false;
+  });
 };
 
 /* eslint-enable no-unused-vars */
