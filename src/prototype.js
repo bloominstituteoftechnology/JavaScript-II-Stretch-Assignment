0d5316b5561933533c6e128565985e9a9767d935
unknown <alexisreyes90@gmail.com>
2018-01-18 20:22:37 -0500

    Alexis Reyes: finished JavaScript-II

--- src/prototype.js
+++ src/prototype.js
@@ -48,7 +48,39 @@
   hamsterHuey.takeDamage(); // returns 'Hamster Huey took damage.'
   hamsterHuey.destroy(); // returns 'Game object was removed from the game.'
 */
+function GameObject(options) {
+  this.createdAt = options.createdAt;
+  this.dimensions = options.dimensions;
+}
 
+GameObject.prototype.destroy = function () {
+  return 'Game object was removed from the game.';
+};
+
+function NPC(options, createdAt, dimensions, hp, name) {
+  GameObject.call(this, options, createdAt, dimensions);
+  this.hp = options.hp;
+  this.name = options.name;
+}
+
+NPC.prototype = Object.create(GameObject.prototype);
+
+NPC.prototype.takeDamage = function () {
+  return `${this.name} took damage.`;
+};
+
+function Humanoid(options, createdAt, dimensions, faction, weapons, language) {
+  NPC.call(this, options, createdAt, dimensions, faction, weapons, language);
+  this.faction = options.faction;
+  this.weapons = options.weapons;
+  this.language = options.language;
+}
+
+Humanoid.prototype = Object.create(NPC.prototype);
+
+Humanoid.prototype.greet = function () {
+  return `${this.name} offers a greeting in ${this.language}.`;
+};
 /* eslint-disable no-undef */
 
 module.exports = {
--- src/this.js
+++ src/this.js
@@ -6,7 +6,15 @@
 
 class User {
   constructor(options) {
+    this.username = options.username;
+    this.password = options.password;
     // set a username and password property on the user object that is created
+  }
+  checkPassword(isPassword) {
+    if (this.password === isPassword) {
+      return true;
+    }
+    return false;
   }
   // create a method on the User class called `checkPassword`
   // this method should take in a string and compare it to the object's password property
@@ -23,17 +31,20 @@ const result = me.checkPassword('correcthorsebatterystaple'); // should return `
 /* part 2 */
 
 const checkPassword = function comparePasswords(passwordToCompare) {
+  return this.password === passwordToCompare;
   // recreate the `checkPassword` method that you made on the `User` class
   // use `this` to access the object's `password` property.
   // do not modify this function's parameters
   // note that we use the `function` keyword and not `=>`
 };
 
+const theRightPassword = 'correcthorsebatterystaple';
 // invoke `checkPassword` on `me` by explicitly setting the `this` context
 // use .call, .apply, and .bind
 
 // .call
-
+checkPassword.call(me, theRightPassword);
 // .apply
-
+checkPassword.apply(me, [theRightPassword]);
 // .bind
+let boundPassword = checkPassword.bind(me);
