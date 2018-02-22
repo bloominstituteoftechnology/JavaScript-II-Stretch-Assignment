1fa596065cd71e5269fd4f57f7d93dceb2d4391e
mgchmiel <michaelchmielniak@yahoo.com>
2018-02-22 17:05:52 -0500

    thank you google

--- src/recursion.js
+++ src/recursion.js
@@ -1,20 +1,44 @@
 // Complete the following functions.
 
 const nFibonacci = (n) => {
-  // fibonacci sequence: 1 1 2 3 5 8 13 ...
+  if (n <= 1) {
+    return n;
+  }
+  return nFibonacci(n - 2) + nFibonacci(n - 1);  // fibonacci sequence: 1 1 2 3 5 8 13 ...
   // return the nth number in the sequence
-  n =
 };
 
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+  if (n <= 1) {
+    return 1;
+  }
+  return n * nFactorial(n - 1);
 };
 
 /* Extra Credit */
 const checkMatchingLeaves = (obj) => {
-  // return true if every property on `obj` is the same
-  // otherwise return false
+  let result = true;
+  const leaves = [];
+  const recur = (objGiven) => {
+    Object.keys(objGiven).forEach((key) => {
+      if (typeof objGiven[key] === 'number' || typeof objGiven[key] === 'string') {
+        leaves.push(objGiven[key]);
+      }
+      if (typeof objGiven[key] === 'object') {
+        recur(objGiven[key]);
+      }
+    });
+  };
+  recur(obj);
+  leaves.reduce((memo, i) => {
+    if (memo !== i) {
+      result = false;
+    }
+    return i;
+  });
+  return result;
 };
 
 /* eslint-enable no-unused-vars */
