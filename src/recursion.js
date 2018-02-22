568e14c65656b3bf75eba3c6aad4db67d901d9ad
zackhitch <zackhitch@users.noreply.github.com>
2018-02-22 14:08:22 -0800

    modified recursion.js - WIP - Zack Hitchcock

--- src/recursion.js
+++ src/recursion.js
@@ -3,9 +3,13 @@
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 1 2 3 5 8 13 ...
   // return the nth number in the sequence
-  if (n <= 1) return 1;
-  return nFibonacci(n - 2) + nFibonacci(n - 1);
+  if (n > 2) {
+    return nFibonacci(n - 2) + nFibonacci(n - 1);
+  }
+  return 1;
 };
+
+// console.log(nFibonacci(5));
 
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
@@ -18,6 +22,7 @@ const nFactorial = (n) => {
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  
 };
 
 /* eslint-enable no-unused-vars */
