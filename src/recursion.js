b7269b1ec605d109d6746c83ee53b9a64949ea1f
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-02-22 15:11:43 -0600

    nFibonacci -incomplete, nFactorial-complete

--- src/recursion.js
+++ src/recursion.js
@@ -3,17 +3,25 @@
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  let result = 0;
+  if (n <= 1) return 1;
+  result += nFibonacci(n - 1) + nFibonacci(n - 2);
+  return result;
 };
 
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+  let result = 1;
+  if (n === result) return result;
+  result = n * nFactorial(n - 1); return result;
 };
 
 /* Extra Credit */
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  
 };
 
 /* eslint-enable no-unused-vars */
