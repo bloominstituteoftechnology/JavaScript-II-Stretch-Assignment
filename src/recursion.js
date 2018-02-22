68d777383356a33a63dab4e7b484728e02a9b2d5
Ajmal Jalal <ajmaljalal58@gmail.com>
2018-02-22 15:50:48 -0800

    Ajmal \ recursion part, the Febonacci solution

--- src/recursion.js
+++ src/recursion.js
@@ -4,8 +4,9 @@ const nFibonacci = (n) => {
   // fibonacci sequence: 1 1 2 3 5 8 13 ...
   // return the nth number in the sequence
   let result = 0;
-  if (n <= 1) return 1;
-  result += nFibonacci(n - 1) + nFibonacci(n - 2);
+  if (n === 0) return 0;
+  if (n <= 2) return 1;
+  result = nFibonacci(n - 1) + nFibonacci(n - 2);
   return result;
 };
 
@@ -19,10 +20,25 @@ const nFactorial = (n) => {
 
 /* Extra Credit */
 const checkMatchingLeaves = (obj) => {
-  // return true if every property on `obj` is the same
-  // otherwise return false
-  
-};
+// return true if every property on `obj` is the same
+// otherwise return false
+/*   let check = false;
+  let keyArray = Object.keys(obj);
+  for (let i = 0; i < keyArray.length; i++) {
+    if (keyArray[i] === 'object') {
+      keyArray = keyArray.concat(checkMatchingLeaves(obj[i]));
+    } else {
+      keyArray.push(obj[i]);
+    }
+  }
+  const firstItem = keyArray[0];
+  for (let i = 0; i < keyArray.length; i++) {
+    if (keyArray[i] === firstItem) {
+      check = true;
+    }
+  }
+  return check;
+}; */
 
 /* eslint-enable no-unused-vars */
 
