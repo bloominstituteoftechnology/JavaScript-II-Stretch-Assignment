34aca089c169bef7b9da8e17cf14bf3efab09810
Charletta <charlettabullard12@outlook.com>
2018-02-23 14:26:53 -0500

    Prototype WIP

--- src/prototype.js
+++ src/prototype.js
@@ -4,24 +4,58 @@
 
   In this file you will be creating three classes:
   GameObject
-    createdAt
+   createdAt
     dimensions
-    destroy() // prototype method -> returns the string 'Game object was removed from the game.'
+    destroy() prototype method -> returns the string 'Game object was removed from the game.'
+    */
+    function GameObject(gameAttrs) {
+      this.createdAt = gameAttrs.createdAt;
+      this.dimensions = gameAttrs.dimensions;
+    }
 
+    GameObject.prototype.destroy = (destroyed) => {
+      return ('Game object was removed from the game.');
+    };
+    /*
   NPC
     hp
     name
-    takeDamage() // prototype method -> returns the string '<object name> took damage.'
-    // should inherit destroy() from GameObject's prototype
+    takeDamage()  prototype method -> returns the string '<object name> took damage.'
+     should inherit destroy() from GameObject's prototype
+    */
+    function NPC(npcAttrs) {
+      GameObject.call(this, npcAttrs);
+      this.hp = npcAttrs.hp;
+      this.name = npcAttrs.name;
+    }
 
+    NPC.prototype.takeDamage = (damage) => {
+      return (`${this.name} took damage.`);
+    };
+  /*
   Humanoid
     faction
     weapons
     language
-    greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
-    // should inherit destroy() from GameObject through NPC
-    // should inherit takeDamage() from NPC
+      greet()  prototype method -> returns the string '<object name> offers a greeting in <object language>.'
+      should inherit destroy() from GameObject through NPC
+      should inherit takeDamage() from NPC
+      */
+    function Humanoid(humAttrs) {
+      GameObject.call(this, humAttrs);
+      NPC.call(this, humAttrs);
+      this.faction = humAttrs.faction;
+      this.name = humAttrs.name;
+      this.weapons = humAttrs.weapons;
+      this.language = humAttrs.language;
+    }
 
+    Humanoid.prototype.greet = (greetType) => {
+      return (`${this.name} offers a greeting in ${this.language}.`);
+    };
+
+
+/*
   Inheritance chain: Humanoid -> NPC -> GameObject
   Instances of Humanoid should have all of the same properties as NPC and GameObject.
   Instances of NPC should have all of the same properties as GameObject.
@@ -49,10 +83,29 @@
   hamsterHuey.destroy(); // returns 'Game object was removed from the game.'
 */
 
+    const javanoid = new Humanoid({
+      createdAt: '2/23/18',
+      dimensions: {
+        length: 2,
+        width: 1,
+        height: 1,
+      },
+      hp: 100,
+      name: 'Javanoid',
+      faction: 'light',
+      weapons: [
+        'laser eyes',
+      ],
+      language: 'French',
+    });
+    javanoid.greet();
+    javanoid.takeDamage();
+    javanoid.destroy();
+
 /* eslint-disable no-undef */
 
-module.exports = {
-  GameObject,
-  NPC,
-  Humanoid,
-};
+    module.exports = {
+      GameObject,
+      NPC,
+      Humanoid,
+    };
