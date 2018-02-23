b170483835c86d901b5859a943e45dd595ede7ec
joshuacoyne <josh.j.coyne@gmail.com>
2018-02-22 22:14:47 -0500

    JavaScript-II--Recursion completed--this completed minus bind

--- src/this.js
+++ src/this.js
@@ -7,10 +7,16 @@
 class User {
   constructor(options) {
     // set a username and password property on the user object that is created
+    this.username = options.user;
+    this.password = options.password;
   }
   // create a method on the User class called `checkPassword`
   // this method should take in a string and compare it to the object's password property
   // return `true` if they match, otherwise return `false`
+  checkPassword(str) {
+    if (str === this.password) return true;
+    return false;
+  }
 }
 
 const me = new User({
@@ -19,6 +25,7 @@ const me = new User({
 });
 
 const result = me.checkPassword('correcthorsebatterystaple'); // should return `true`
+// console.log(result);
 
 /* part 2 */
 
@@ -27,13 +34,21 @@ const checkPassword = function comparePasswords(passwordToCompare) {
   // use `this` to access the object's `password` property.
   // do not modify this function's parameters
   // note that we use the `function` keyword and not `=>`
+  // if (passwordToCompare === '1234') return true;
+  // return false;
+
+  if (this.password === passwordToCompare) return true;
+  return false;
 };
+
 
 // invoke `checkPassword` on `me` by explicitly setting the `this` context
 // use .call, .apply, and .bind
 
 // .call
+// console.log(checkPassword.call(me, 'correcthorsebatterystaple'));
 
 // .apply
+// console.log(checkPassword.apply(me, ['correcthorsebatterystaple']));
 
 // .bind
