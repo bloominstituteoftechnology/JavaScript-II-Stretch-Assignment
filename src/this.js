5b9b0359c722b9b6d711cdf50eeed10a1f5a5a3a
Carlos Andrade <carlos.andrade86@outlook.com>
2018-02-22 19:55:02 -0500

    Carlos Andrade, Robert Rak, Viveca Valdez, Rachelle De Jesus Pestanas--JavaScript-II-This-COMPLETED

--- src/this.js
+++ src/this.js
@@ -5,12 +5,23 @@
 /* part 1 */
 
 class User {
-  constructor(options) {
+  constructor(username, password) {
+    this.username = username;
+    this.password = password;
     // set a username and password property on the user object that is created
   }
+  checkPassword(password) {
+    if (this.password === password){
+         return true;
+    }else {
+        return false;
+    }
+  }
+
   // create a method on the User class called `checkPassword`
   // this method should take in a string and compare it to the object's password property
   // return `true` if they match, otherwise return `false`
+
 }
 
 const me = new User({
