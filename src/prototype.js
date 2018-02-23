fe40f76f5b08a92564a994abc68fd3f6bb14d95e
Johnathan Huggett <johnathanhuggett@Johnathans-MacBook-Pro.local>
2018-02-23 15:02:51 -0500

    Johnathan Huggett - Edward Manda prototype transformed into class typeJS II

--- src/prototype.js
+++ src/prototype.js
@@ -8,6 +8,8 @@
     dimensions
     destroy() // prototype method -> returns the string 'Game object was removed from the game.'
 */
+// Constructor way of creating a game obj
+/* 
 function GameObject(options) {
   this.createdAt = options.createdAt;
   this.dimensions = options.dimensions;
@@ -16,7 +18,19 @@ function GameObject(options) {
 GameObject.prototype.destroy = function () {
   return 'Game object was removed from the game';
 };
+*/
+// Using a class
 
+class GameObject {
+  constructor (options) {
+    this.createdAt = options.createdAt;
+    this.dimensions = options.dimensions;
+  }
+  destroy() {
+    return 'Game object was removed from the game';
+  }
+}
+  
 /*
   NPC
     hp
@@ -24,6 +38,8 @@ GameObject.prototype.destroy = function () {
     takeDamage() // prototype method -> returns the string '<object name> took damage.'
     // should inherit destroy() from GameObject's prototype
 */
+// Constructor way of creating a NPC obj
+/*
 function NPC(options) {
   GameObject.call(this, options);
   this.hp = options.hp;
@@ -35,7 +51,18 @@ NPC.prototype = Object.create(GameObject.prototype);
 NPC.prototype.takeDamage = function () {
   return `${this.name} took damage`;
 };
-
+*/
+// Using a class
+class NPC extends GameObject {
+  constructor (options) {
+    super (options);
+    this.hp = options.hp;
+    this.name = options.name;
+  }
+  takeDamage() {
+    return `${this.name} took damage`;
+  }
+}
 /*
   Humanoid
     faction
@@ -45,7 +72,8 @@ NPC.prototype.takeDamage = function () {
     // should inherit destroy() from GameObject through NPC
     // should inherit takeDamage() from NPC
 */
-
+// Constructor way of creating a Humanoid obj
+/*
 function Humanoid(attrs) {
   NPC.call(this, attrs);
   this.faction = attrs.faction;
@@ -58,6 +86,19 @@ Humanoid.prototype = Object.create(NPC.prototype);
 Humanoid.prototype.greet = function (name) {
   return `${this.name} offers a greeting in ${this.language}.`;
 };
+*/
+// Using a class
+class Humanoid extends NPC {
+  constructor (attrs) {
+    super(attrs);
+    this.faction = attrs.faction;
+    this.weapons = attrs.weapons;
+    this.language = attrs.language;
+  }
+  greet() {
+    return `${this.name} offers a greeting in ${this.language}.`;
+  }
+}
 
 /*
   Inheritance chain: Humanoid -> NPC -> GameObject
