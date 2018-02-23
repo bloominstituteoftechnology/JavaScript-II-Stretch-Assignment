bb79f6e6604488330e863c93aa42ec78be0941da
William Pelton <williamrpelton@gmail.com>
2018-02-23 13:03:53 -0600

    William Pelton Shaun Kelly WIP

--- src/this.js
+++ src/this.js
@@ -7,6 +7,12 @@
 class User {
   constructor(options) {
     // set a username and password property on the user object that is created
+    this.username = options.username;
+    this.password = options.password;
+    this.checkPassword = function (str) {
+      if (str === this.password) return true;
+      return false;
+    };
   }
   // create a method on the User class called `checkPassword`
   // this method should take in a string and compare it to the object's password property
@@ -27,13 +33,19 @@ const checkPassword = function comparePasswords(passwordToCompare) {
   // use `this` to access the object's `password` property.
   // do not modify this function's parameters
   // note that we use the `function` keyword and not `=>`
+    if (passwordToCompare === this.password) return true;
+    return false;
 };
-
 // invoke `checkPassword` on `me` by explicitly setting the `this` context
 // use .call, .apply, and .bind
 
 // .call
+console.log(checkPassword.call(me,'correcthorsebatterystaple'));
 
 // .apply
+console.log(checkPassword.apply(me,['correcthorsebatterystaple']));
+
 
 // .bind
+const checkCheck = checkPassword.bind(me);
+console.log(checkCheck('correcthorsebatterystaple'));
