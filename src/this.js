1572e45cfd4191d8221e6530586f3397f8a7c23a
Nick Coultas <nncoultas@yahoo.com>
2018-02-22 17:01:06 -0800

    Finished this.js

--- src/this.js
+++ src/this.js
@@ -20,7 +20,7 @@ const me = new User({
 });
 
 const result = me.checkPassword('correcthorsebatterystaple'); // should return `true`
-console.log(result);
+//console.log(result);
 /* part 2 */
 
 const checkPassword = function comparePasswords(passwordToCompare) {
@@ -28,13 +28,19 @@ const checkPassword = function comparePasswords(passwordToCompare) {
   // use `this` to access the object's `password` property.
   // do not modify this function's parameters
   // note that we use the `function` keyword and not `=>`
+    console.log(this.password)
+    console.log(passwordToCompare)
+    return (this.password === passwordToCompare);
 };
-
 // invoke `checkPassword` on `me` by explicitly setting the `this` context
 // use .call, .apply, and .bind
 
 // .call
+console.log(checkPassword.call(me, 'correcthorsebatterystaple'));
 
 // .apply
+console.log(checkPassword.apply(me, ['correcthorsebatterystaple']))
 
 // .bind
+const pwCheck = checkPassword.bind(me);
+console.log(pwCheck('correcthorsebatterystaple'));
