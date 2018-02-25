c3a8eb929d5f3230a0f344fbc04e4452129cfa93
Sumayyah <sumayyah@zubairs-air.attlocal.net>
2018-02-25 13:50:04 -0800

    classes finished

--- src/recursion.js
+++ src/recursion.js
@@ -5,6 +5,7 @@ const nFibonacci = (n) => {
   // return the nth number in the sequence
   if (n < 3) return 1;
   return nFibonacci(n - 1) + nFibonacci(n - 2);
+  // when n = 1 and 2 it returns 1 so thats why you have to put it as an exception first
 };
 
 const nFactorial = (n) => {
@@ -13,7 +14,7 @@ const nFactorial = (n) => {
   if (n < 2) return 1;
   return n * nFactorial(n - 1);
 };
-
+// n < 2 works because both 1! and 0! return 1
 /* Extra Credit */
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
