7fd54937a8eccead3bbd885e20358b5efa475c2e
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-02-25 19:46:09 -0600

    completed this.js and redid prototype.js after rewatching lectures and better understanding

--- src/prototype.js
+++ src/prototype.js
@@ -1,19 +1,16 @@
 /*
   Object oriented design is commonly used in video games.  For this part of the assignment
   you will be implementing several classes with their correct inheritance heirarchy.
-
   In this file you will be creating three classes:
   GameObject
     createdAt
     dimensions
     destroy() // prototype method -> returns the string 'Game object was removed from the game.'
-
   NPC
     hp
     name
     takeDamage() // prototype method -> returns the string '<object name> took damage.'
     // should inherit destroy() from GameObject's prototype
-
   Humanoid
     faction
     weapons
@@ -21,13 +18,10 @@
     greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
     // should inherit destroy() from GameObject through NPC
     // should inherit takeDamage() from NPC
-
   Inheritance chain: Humanoid -> NPC -> GameObject
   Instances of Humanoid should have all of the same properties as NPC and GameObject.
   Instances of NPC should have all of the same properties as GameObject.
-
   Example:
-
   const hamsterHuey = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -43,48 +37,52 @@
     ],
     language: 'Hamsterish',
   });
-
   hamsterHuey.greet(); // returns 'Hamster Huey offers a greeting in Hamsterish'
   hamsterHuey.takeDamage(); // returns 'Hamster Huey took damage.'
   hamsterHuey.destroy(); // returns 'Game object was removed from the game.'
 */
 
-/* eslint-disable no-undef */
+// ##################### GAMEOBJECT ######################
 
-// ####################### GameObject #######################
-function GameObject(g) {
-  this.createdAt = g.createdAt;
-  this.dimensions = g.dimensions;
+function GameObject(options) {
+  this.createdAt = new Date();
+  this.dimensions = options.dimensions;
+  this.length = options.dimensions.length;
+  this.width = options.dimensions.width;
+  this.height = options.dimensions.height;
 }
-GameObject.prototype.destroy = function (item) {  // parameter ??
+
+GameObject.prototype.destroy = function () {
   return ('Game object was removed from the game.');
 };
-// ####################### NPC #######################
 
-function NPC(n) {
-  GameObject.call(this, n);
-  this.hp = n.hp;
-  this.name = n.name;
+
+// ##################### NPC ######################
+
+function NPC(options) {
+  GameObject.call(this, options);
+  this.hp = options.hp;
+  this.name = options.name;
 }
 NPC.prototype = Object.create(GameObject.prototype);
-
-NPC.prototype.takeDamage = function (td) { // parameter ??
+NPC.prototype.takeDamage = function () {
   return (`${this.name} took damage.`);
 };
-// ####################### HUMANOID #######################
-function Humanoid(h) {
-  NPC.call(this, h);
-  this.faction = h.faction;
-  this.weapons = h.weapons;
-  this.language = h.language;
+
+// ##################### HUMANOID ######################
+
+function Humanoid(options) {
+  NPC.call(this, options);
+  this.faction = options.faction;
+  this.weapons = options.weapons;
+  this.language = options.language;
 }
-
 Humanoid.prototype = Object.create(NPC.prototype);
-
-Humanoid.prototype.greet = function (g) { // parameter ??
+Humanoid.prototype.greet = function () {
   return (`${this.name} offers a greeting in ${this.language}.`);
 };
 
+/* eslint-disable no-undef */
 
 module.exports = {
   GameObject,
