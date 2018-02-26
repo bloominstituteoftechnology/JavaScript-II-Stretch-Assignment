936f6ecbb006c8f9cb5677f745b392292d806743
Daniel Plott <Daniel.P.Plott@gmail.com>
2018-02-25 21:13:24 -0600

    completed recursion (including XC) and 'this'

--- src/recursion.js
+++ src/recursion.js
@@ -7,17 +7,36 @@ const nFibonacci = (n) => {
   return nFibonacci(n - 1) + nFibonacci(n - 2);
 };
 
+
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+  if (n === 1) return 1;
+  return n * nFactorial(n - 1);
 };
 
-/* Extra Credit */
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  let test;
+  let flag = true;
+  const checkLeaves = (object) => {
+    Object.keys(object).forEach((key) => {
+      if (test === undefined && object[key] !== 'object') {
+        test = object[key];
+        return undefined;
+      }
+      if (typeof object[key] === 'object') return checkLeaves(object[key]);
+      if (object[key] !== test) {
+        flag = false;
+        return undefined;
+      }
+    });
+    return undefined;
+  };
+  checkLeaves(obj);
+  return flag;
 };
-
 /* eslint-enable no-unused-vars */
 
 module.exports = {
