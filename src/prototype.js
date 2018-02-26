d2aef372540b81515ade8822c3d2f2b19630f5ee
KJ Sourcing <tmkogwork@gmail.com>
2018-02-26 02:17:58 -0800

    js II complete

--- src/prototype.js
+++ src/prototype.js
@@ -6,48 +6,120 @@
   GameObject
     createdAt
     dimensions
-    destroy() // prototype method -> returns the string 'Game object was removed from the game.'
+    destroy() // prototype method -> returns the string 'Game object was removed from the game.' */
 
-  NPC
+// class GameObject {
+//   constructor(object) {
+//     this.createdAt = object.createdAt;
+//     this.dimensions = object.dimensions;
+//   }
+//   destroy() {
+//     return (this.message = 'Game object was removed from the game.');
+//   }
+// }
+
+function GameObject(object) {
+  this.createdAt = object.createdAt;
+  this.dimensions = object.dimensions;
+}
+
+GameObject.prototype.destroy = function () {
+  return 'Game object was removed from the game.';
+};
+
+
+   /* NPC
     hp
     name
     takeDamage() // prototype method -> returns the string '<object name> took damage.'
-    // should inherit destroy() from GameObject's prototype
+     should inherit destroy() from GameObject's prototype */
 
-  Humanoid
+// class NPC extends GameObject {
+//   constructor(object) {
+//     super(object);
+//     this.hp = object.hp;
+//     this.name = object.name;
+//   }
+//   takeDamage() {
+//     return `${this.name} took damage.`;
+//   }
+// }
+
+function NPC(attrs) {
+  GameObject.call(this, attrs);
+  this.hp = attrs.hp;
+  this.name = attrs.name;
+}
+
+NPC.prototype = Object.create(GameObject.prototype);
+
+NPC.prototype.takeDamage = function () {
+  return `${this.name} took damage.`;
+};
+
+
+  /* Humanoid
     faction
     weapons
     language
     greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
     // should inherit destroy() from GameObject through NPC
-    // should inherit takeDamage() from NPC
+    // should inherit takeDamage() from NPC */
 
+// class Humanoid extends NPC {
+//   constructor(object) {
+//     super(object);
+//     this.faction = object.faction;
+//     this.weapons = object.weapons;
+//     this.language = object.language;
+//   }
+
+//   greet() {
+//     return `${this.name} offers a greeting in ${this.language}.`;
+//   }
+// }
+
+function Humanoid(attrs) {
+  NPC.call(this, attrs);
+  this.faction = attrs.faction;
+  this.weapons = attrs.weapons;
+  this.language = attrs.language;
+}
+
+Humanoid.prototype = Object.create(NPC.prototype);
+
+Humanoid.prototype.greet = function () {
+  return `${this.name} offers a greeting in ${this.language}.`;
+};
+
+   /*
   Inheritance chain: Humanoid -> NPC -> GameObject
   Instances of Humanoid should have all of the same properties as NPC and GameObject.
   Instances of NPC should have all of the same properties as GameObject.
 
   Example:
+  */
 
-  const hamsterHuey = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    hp: 5,
-    name: 'Hamster Huey',
-    faction: 'Gooey Kablooie',
-    weapons: [
-      'bubblegum',
-    ],
-    language: 'Hamsterish',
-  });
+const hamsterHuey = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1,
+  },
+  hp: 5,
+  name: 'Hamster Huey',
+  faction: 'Gooey Kablooie',
+  weapons: [
+    'bubblegum',
+  ],
+  language: 'Hamsterish',
+});
 
-  hamsterHuey.greet(); // returns 'Hamster Huey offers a greeting in Hamsterish'
-  hamsterHuey.takeDamage(); // returns 'Hamster Huey took damage.'
-  hamsterHuey.destroy(); // returns 'Game object was removed from the game.'
-*/
+hamsterHuey.greet(); // returns 'Hamster Huey offers a greeting in Hamsterish'
+hamsterHuey.takeDamage(); // returns 'Hamster Huey took damage.'
+hamsterHuey.destroy(); // returns 'Game object was removed from the game.'
+
 
 /* eslint-disable no-undef */
 
--- src/recursion.js
+++ src/recursion.js
@@ -3,18 +3,76 @@
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  if (n === 0 || n === 1) return n;
+  return nFibonacci(n - 1) + nFibonacci(n - 2);
 };
+
 
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+  if (n === 0) {
+    return 1;
+  }
+  return n * nFactorial(n - 1);
 };
 
 /* Extra Credit */
-const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  // values in one object are the same
+  // compare the value
+  // find the same values and return true or false
+  // how do I get to the value on the property of the object???
+// const checkMatchingLeaves = (obj) => {
+
+const checkMatchingLeaves = (obj) => {
+  let flag = true;
+  let value;
+  const compare = (object) => {
+    Object.keys(object).forEach((key) => {
+      if (value === undefined && typeof object[key] !== 'object') {
+        value = object[key];
+        return;
+      }
+      if (typeof object[key] === 'object') return compare(object[key]);
+      if (object[key] !== value) {
+        flag = false;
+        return;
+      }
+      return;
+    });
+  };
+  compare(obj);
+  return flag;
 };
+
+const objectCompare = { notSame: 2, stillDifferent: 3 };
+
+// const checkMatchingLeaves = (obj) => {
+//   let value;
+//   let allMatch = true;
+
+//   const checkLeaves = (object) => {
+//     Object.keys(object).forEach((key) => {
+//       if (value === undefined && typeof object[key] !== 'object') { // will only ever run once
+//         value = object[key];
+//         return; // breaks out of forEach
+//       }
+//       if (typeof object[key] === 'object') return checkLeaves(object[key]);
+//       if (object[key] !== value) {
+//         allMatch = false;
+//         return; // breaks out of forEach
+//       }
+//       return; // breaks out of forEach
+//     });
+//   };
+//   checkLeaves(obj);
+//   return allMatch;
+// };
+
+// const objectCompare = { notSame: 2, stillDifferent: 3 };
+// console.log(checkMatchingLeaves(1));
 
 /* eslint-enable no-unused-vars */
 
