412a41a9dee4098c0159c9e63b63a9d4c4454798
Axhon <axhonruiz@gmail.com>
2018-03-19 17:12:38 -1000

    recursion- all but e.c. done

--- src/recursion.js
+++ src/recursion.js
@@ -3,17 +3,26 @@
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  if (n <= 2) return 1;
+  return nFibonacci(n - 1) + nFibonacci(n - 2);
 };
 
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+  if (n === 0) return 1;
+  return n * nFactorial(n - 1);
 };
+
+// flatten object
 
 /* Extra Credit */
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  const t = new Set(Object.values(obj));
+  if (t.size === 1) return true;
+  return false;
 };
 
 /* eslint-enable no-unused-vars */
