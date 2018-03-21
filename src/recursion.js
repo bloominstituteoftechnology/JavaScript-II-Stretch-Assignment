5c5d42f784f4c337be414d6d96382c85eb218fc6
Anthony <anthony@anthonys-mbp.lan>
2018-03-21 17:19:27 -0400

    Recursion done, minus EC

--- src/recursion.js
+++ src/recursion.js
@@ -3,11 +3,29 @@
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  if (n <= 2) {
+    return 1;
+  }
+  let result = 0;
+  let counter = 2;
+  let prev = 1;
+  let prevPrev = 1;
+  while (counter < n) {
+    result = prev + prevPrev;
+    const temp = prev;
+    prev = result;
+    prevPrev = temp;
+    counter++;
+  }
+  return result;
 };
 
 const nFactorial = (n) => {
-  // factorial example: !5 = 5 * 4 * 3 * 2 * 1
-  // return the factorial of `n`
+  const o = n - 1;
+  if (n === 1) {
+    return n;
+  }
+  return n * nFactorial(o);
 };
 
 /* Extra Credit */
