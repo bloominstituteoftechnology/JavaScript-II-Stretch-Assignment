9d249a4a539a643236e69c86ff516703742dfb67
Devon Bacon <devon.bacon@gmail.com>
2018-03-21 13:55:45 -0700

    Completed part 2 of this.js

--- src/this.js
+++ src/this.js
@@ -32,13 +32,17 @@ const checkPassword = function comparePasswords(passwordToCompare) {
   // use `this` to access the object's `password` property.
   // do not modify this function's parameters
   // note that we use the `function` keyword and not `=>`
+  return this.password === passwordToCompare;
 };
 
 // invoke `checkPassword` on `me` by explicitly setting the `this` context
 // use .call, .apply, and .bind
 
 // .call
+checkPassword.call(me, 'correcthorsebatterystaple');
 
 // .apply
+checkPassword.apply(me, ['correcthorsebatterystaple']);
 
 // .bind
+checkPassword.bind(me, 'correcthorsebatterystaple')();
