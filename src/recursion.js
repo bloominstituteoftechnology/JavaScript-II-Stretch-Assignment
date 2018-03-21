a3b2ae234a5f7853d6ecf71dab89f680326bdf31
Axhon <axhonruiz@gmail.com>
2018-03-20 15:29:16 -1000

    all but last test green

--- src/recursion.js
+++ src/recursion.js
@@ -15,12 +15,52 @@ const nFactorial = (n) => {
 };
 
 // flatten object
+// TODO
+// var flattenObject = function(ob) {
+//   return Object.keys(ob).reduce(function(toReturn, k) {
+//     if (Object.prototype.toString.call(ob[k]) === '[object Date]') {
+//       toReturn[k] = ob[k].toString();
+//     } else if (typeof ob[k] === 'object' && ob[k]) {
+//       var flatObject = flattenObject(ob[k]);
+//       Object.keys(flatObject).forEach(function(k2) {
+//         toReturn[k + '.' + k2] = flatObject[k2];
+//       });
+//     } else {
+//       toReturn[k] = ob[k];
+//     }
+
+//     return toReturn;
+//   }, {});
+// };
+// var now = new Date();
+// var o = {
+//   a: 4,
+//   b: 'string b',
+//   c: {
+//     d: null,
+//     e: 3,
+//     f: {
+//       g: 'string g',
+//       h: ['in', 'an', 'array', { i: 'sneaky string in object of array' }],
+//       i: undefined,
+//     },
+//     j: now,
+//   },
+// };
+// console.log(flattenObject(o));
+const flattenObject = nestedObject => nestedObject;
 
 /* Extra Credit */
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+
+  // flatten object
+  flattenObject(Object.assign({}, obj));
+
+  // obj needs to be flattened
   const t = new Set(Object.values(obj));
+
   if (t.size === 1) return true;
   return false;
 };
