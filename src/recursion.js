9a3882db2a302e95f548daaa45ee1542447adb1c
Robert Hogan <rhogan29@outlook.com>
2018-03-21 19:01:58 -0400

    Robert Hogan. Further work on recursion and this.

--- src/recursion.js
+++ src/recursion.js
@@ -3,12 +3,26 @@
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  if (n <= 1) {
+    return n;
+  } else {
+    return nFibonacci(n -1) + nFibonacci(n - 2);
+  }
 };
+
+console.log(nFibonacci(4));
 
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * nFactorial(n -1);
+  }
 };
+
+console.log(nFactorial(5));
 
 /* Extra Credit */
 const checkMatchingLeaves = (obj) => {
