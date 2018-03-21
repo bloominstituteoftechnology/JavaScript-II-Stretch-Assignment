67bf0125207b80d499dde2846fb7c61e0ab7d472
odax <nr10@albion.edu>
2018-03-21 19:06:12 -0400

    completed recursion.js

--- src/recursion.js
+++ src/recursion.js
@@ -3,11 +3,21 @@
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  let arr = [1, 1, 2];
+  for (i = 2; arr.length < (n + 2); i + arr[i++]) {
+   i = arr[arr.length - 1] + arr[arr.length - 2];
+   arr.push(i);
+  }
+  return arr[n];
 };
 
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+  if (n != 1) {
+    return n * nFactorial(n-1); 
+  }
+  else return 1;
 };
 
 /* Extra Credit */
