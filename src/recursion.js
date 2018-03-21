afb61de35310b666eeb3a47bc6abc8f7fac0c1b2
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-03-21 18:59:00 -0400

    completed this and recursion

--- src/recursion.js
+++ src/recursion.js
@@ -1,13 +1,17 @@
-// Complete the following functions.
+// // Complete the following functions.
 
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  if (n <= 2) return 1;
+  return (nFibonacci(n - 1) + nFibonacci(n - 2));
 };
 
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+  if (n <= 1) return 1;
+  return nFactorial(n - 1) * n;
 };
 
 /* Extra Credit */
@@ -23,3 +27,4 @@ module.exports = {
   nFactorial,
   checkMatchingLeaves,
 };
+
