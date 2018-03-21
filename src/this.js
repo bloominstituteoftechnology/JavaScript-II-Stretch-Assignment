382b7ad37025b893047dc38afb9ccaf7d61a3e99
Justin M <jekm321@gmail.com>
2018-03-21 17:15:49 -0400

    finished up JS-II this and most of recursion, up to e.c.

--- src/this.js
+++ src/this.js
@@ -6,7 +6,13 @@
 
 class User {
   constructor(options) {
+    this.username = options.username;
+    this.password = options.password;
     // set a username and password property on the user object that is created
+  }
+
+  checkPassword(pass) {
+    if (this.password === pass) return true;
   }
   // create a method on the User class called `checkPassword`
   // this method should take in a string and compare it to the object's password property
@@ -27,13 +33,17 @@ const checkPassword = function comparePasswords(passwordToCompare) {
   // use `this` to access the object's `password` property.
   // do not modify this function's parameters
   // note that we use the `function` keyword and not `=>`
+  if (this.password === passwordToCompare) return true;
 };
 
 // invoke `checkPassword` on `me` by explicitly setting the `this` context
 // use .call, .apply, and .bind
 
 // .call
+checkPassword.call(me.password);
 
 // .apply
+checkPassword.apply(me.password);
 
 // .bind
+checkPassword.bind(me);
