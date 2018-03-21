187455ff11a5d87d870e167af7e798bd481b47fe
Justin M <jekm321@gmail.com>
2018-03-21 18:04:19 -0400

    fixed up this.js

--- src/this.js
+++ src/this.js
@@ -12,7 +12,7 @@ class User {
   }
 
   checkPassword(pass) {
-    if (this.password === pass) return true;
+    return (this.password === pass);
   }
   // create a method on the User class called `checkPassword`
   // this method should take in a string and compare it to the object's password property
@@ -33,17 +33,19 @@ const checkPassword = function comparePasswords(passwordToCompare) {
   // use `this` to access the object's `password` property.
   // do not modify this function's parameters
   // note that we use the `function` keyword and not `=>`
-  if (this.password === passwordToCompare) return true;
+  return (this.password === passwordToCompare);
 };
 
 // invoke `checkPassword` on `me` by explicitly setting the `this` context
 // use .call, .apply, and .bind
 
 // .call
-checkPassword.call(me.password);
+checkPassword.call(me, me.password);
 
 // .apply
-checkPassword.apply(me.password);
+const arr = [me.password];
+checkPassword.apply(me, arr);
 
 // .bind
-checkPassword.bind(me);
+const newMe = checkPassword.bind(me);
+newMe(me.password);
