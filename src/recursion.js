311d002a98cf851457f2bd5cff4740c5a8e3822f
Jared Cuffe <jcuffe@gmail.com>
2018-03-21 11:21:12 -0700

    `this` and recursion done, still working on recursion stretch. Assigned work for `this` is low-quality in comparison to the work so far.

--- src/recursion.js
+++ src/recursion.js
@@ -3,17 +3,42 @@
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  if (n === 1 || n === 2) {
+    return 1;
+  }
+
+  return nFibonacci(n - 2) + nFibonacci(n - 1);
 };
 
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+  if (n === 1) {
+    return 1;
+  }
+
+  return n * nFactorial(n - 1);
 };
 
 /* Extra Credit */
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  const keys = Object.keys(obj);
+  if (keys.length === 1) {
+    return true;
+  }
+
+  if (typeof obj[keys[0]] === 'object') {
+    return checkMatchingLeaves(Object.assign(obj, obj[keys[0]]));
+  }
+
+  if (obj[keys[0]] !== obj[keys[1]]) {
+    return false;
+  }
+
+  delete obj[keys[0]];
+  return checkMatchingLeaves(obj);
 };
 
 /* eslint-enable no-unused-vars */
