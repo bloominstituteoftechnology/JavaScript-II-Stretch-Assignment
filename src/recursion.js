b6fda9591c0b62aaa5fffb0387bc98ed2dd00e14
nickbrenn <nbren@tutanota.com>
2018-03-21 17:49:33 -0400

    Finished recursion

--- src/recursion.js
+++ src/recursion.js
@@ -3,11 +3,20 @@
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  const myFib = [1, 1, 2];
+  for (let i; myFib.length < n + 1; i + myFib[i++]) {
+    i = myFib[myFib.length - 1] + myFib[myFib.length - 2];
+    myFib.push(i);
+  }
+  return myFib[--n];
 };
 
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+  if (n === undefined) n = 1;
+  if (n === 1) return n;
+  return n * nFactorial(--n);
 };
 
 /* Extra Credit */
