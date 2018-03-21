63adbb8e8639cbc9fa50d0cfd4a527928288e790
Nedu Robert <nedumrobert@gmail.com>
2018-03-21 23:01:54 +0100

    completed recursion and this projects

--- src/recursion.js
+++ src/recursion.js
@@ -3,17 +3,26 @@
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  if (n <= 2) {
+    return 1;
+  }
+  return nFibonacci(n - 1) + nFibonacci(n - 2);
 };
 
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+  const result = n - 1;
+  if (n === 0) {
+    return 1;
+  }
+  return n * nFactorial(result);
 };
 
 /* Extra Credit */
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
-  // otherwise return false
+  // otherwise return false 
 };
 
 /* eslint-enable no-unused-vars */
