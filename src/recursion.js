1f9a6fe86138990d1a3e4a0a567b01998d6c315d
Cruise Brantley <cruise.brantley@gmail.com>
2018-03-22 17:30:26 -0500

    fixed fibFunction, had an issue with skipping a number and being off

--- src/recursion.js
+++ src/recursion.js
@@ -6,19 +6,26 @@ const nFibonacci = (n) => {
   let num1 = 1;
   let num2 = 2;
   let sum = 3;
-
+  if (n === 0) {
+    return 0;
+  }
   if (n === 1) {
     return 1;
   }
   if (n === 2) {
+    return 1;
+  }
+  if (n === 3) {
     return 2;
   }
-  function fibFunction() {
+
+
+  function fibFunction(m) {
     sum = num1 + num2;
     num1 = num2;
     num2 = sum;
-    if (n > 3) {
-      fibFunction(n - 1);
+    if (m > 4) {
+      fibFunction(m - 2);
     }
     return sum;
   }
