0c867d5f885cdc4de3403d4e64e2241769ef7d16
Matthias Bakken <matthias.bakken@gmail.com>
2018-03-21 17:01:14 -0700

    finished recursion and this

--- src/recursion.js
+++ src/recursion.js
@@ -3,12 +3,18 @@
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  // n - 6 (6th number in sequence is 8)... (n-1) + (n-2)... 5th + 4th... 5 + 3 = 8
+  return (n < 2) ? n : nFibonacci(n - 2) + nFibonacci(n - 1);
 };
 
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
-};
+  let result = 1;
+  if (n === 1 || n === 0) return n;
+  result *= n * nFactorial(n - 1);
+  return result;
+  };
 
 /* Extra Credit */
 const checkMatchingLeaves = (obj) => {
