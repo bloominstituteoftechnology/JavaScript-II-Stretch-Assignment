4c2e17bd5ab7513f8548a1a20aa63588e282fa91
markstez05 <36493764+markstez05@users.noreply.github.com>
2018-03-21 23:40:03 -0500

    finished THIS need to finish recursion and mini/ lost time due to eslint errors

--- src/this.js
+++ src/this.js
@@ -5,10 +5,19 @@
 /* part 1 */
 
 class User {
-  constructor(options) {
+  constructor(username, password) {
     // set a username and password property on the user object that is created
-  }
+    this.username = username;
+    this.password = password;
   // create a method on the User class called `checkPassword`
+    this.checkPassword = function() {
+    if (password === this.password) {
+    return true;
+  } else {
+  return false;
+  }
+}
+  }
   // this method should take in a string and compare it to the object's password property
   // return `true` if they match, otherwise return `false`
 }
@@ -19,7 +28,7 @@ const me = new User({
 });
 
 const result = me.checkPassword('correcthorsebatterystaple'); // should return `true`
-
+console.log(result)
 /* part 2 */
 
 const checkPassword = function comparePasswords(passwordToCompare) {
@@ -27,13 +36,20 @@ const checkPassword = function comparePasswords(passwordToCompare) {
   // use `this` to access the object's `password` property.
   // do not modify this function's parameters
   // note that we use the `function` keyword and not `=>`
+  if (this.password === passwordToCompare) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // invoke `checkPassword` on `me` by explicitly setting the `this` context
 // use .call, .apply, and .bind
 
 // .call
-
+console.log(checkPassword.call(me));
 // .apply
-
+console.log(checkPassword.apply(me));
 // .bind
+let bindFunction = checkPassword.apply(me);
+console.log(bindFunction);
