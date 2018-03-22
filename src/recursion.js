265abdc4b2953874eab6fa08e7c4f888f70f27d2
Devon Bacon <devon.bacon@gmail.com>
2018-03-22 12:45:54 -0700

    Completed extra credit for recursion.js

--- src/recursion.js
+++ src/recursion.js
@@ -17,11 +17,26 @@ const nFactorial = (n) => {
 };
 
 /* Extra Credit */
-const checkMatchingLeaves = (obj) => {
-  // return true if every property on `obj` is the same
-  // otherwise return false
-};
+// Recursive leaf check
+const checkMatchingLeaves = (obj, arr) => { // Pass in array so as not to reset it with recursion
+  // Set up new array
+  arr = arr || [];
+  // Store reference to object values array
+  const objVals = Object.values(obj);
 
+  // Loop through items in values array
+  for (let i = 0; i < objVals.length; i++) {
+    if (typeof objVals[i] === 'object') { // If item is an object then call recursion
+      checkMatchingLeaves(objVals[i], arr);
+    } else { // Else store value in new array
+      arr.push(objVals[i]);
+    }
+  }
+  // Convert arr to set to elimintate duplicates
+  // Convert back to array and check length
+  // If length is 1 then all values were identical
+  return Array.from(new Set(arr)).length === 1;
+};
 
 /* eslint-enable no-unused-vars */
 
