f7ca5b178bf9d1fbfa658deed2858a20fc69c3c4
nickbrenn <nbren@tutanota.com>
2018-03-21 22:54:35 -0400

    Got rid of needless .pushing by making arr be me.password to start with

--- src/this.js
+++ src/this.js
@@ -44,12 +44,10 @@ const checkPassword = function comparePasswords(passwordToCompare) {
 // .call
 checkPassword.call(me,'correcthorsebatterystaple');
 // .apply
-let arr = [];
-arr.push(me.password);
+let arr = [me.password];
 checkPassword.apply(me, arr);
 // .bind
 //let arr = [];
-arr.push(me.password);
 let binder = checkPassword.bind(me, ...arr);
 binder();
 //Pretty sure I am doing .apply and .bind in a weird contrived way
