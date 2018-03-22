9de54cbd7e261d25c25a1b5fac6c102acbc02efe
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-03-22 12:33:52 -0700

    filled out prototype constuctors to pass npm tests

--- src/prototype.js
+++ src/prototype.js
@@ -49,39 +49,37 @@
   hamsterHuey.destroy(); // returns 'Game object was removed from the game.'
 */
 
-function GameObject(attributes) {
-  this.createdAt = attributes.createdAt;
-  this.dimentions = attributes.dimentions;
+function GameObject(properties) {
+  this.createdAt = properties.createdAt;
+  this.dimensions = properties.dimensions;
 }
 GameObject.prototype.destroy = function () {
   return 'Game object was removed from the game.';
 };
 
-function NPC(secondAttributes) {
-  GameObject.call(this, secondAttributes);
-  this.hp = secondAttributes.hp;
-  this.name = secondAttributes.name;
-  this.takeDamage = secondAttributes.takeDamage;
+function NPC(properties) {
+  GameObject.call(this, properties);
+  this.hp = properties.hp;
+  this.name = properties.name;
 }
+
 NPC.prototype = Object.create(GameObject.prototype);
+
 NPC.prototype.takeDamage = function () {
-  if (this.takeDamage) {
-    return `${this.name} took damage.`;
-  }
+  return `${this.name} took damage.`;
 };
 
-function Humanoid(thirdAttributes) {
-  NPC.call(this, thirdAttributes);
-  this.faction = thirdAttributes.faction;
-  this.weapons = thirdAttributes.weapons;
-  this.language = thirdAttributes.language;
-  this.greet = thirdAttributes.greet;
+function Humanoid(properties) {
+  NPC.call(this, properties);
+  this.faction = properties.faction;
+  this.weapons = properties.weapons;
+  this.language = properties.language;
 }
+
 Humanoid.prototype = Object.create(NPC.prototype);
+
 Humanoid.prototype.greet = function () {
-  if (this.greet) {
-    return `${this.name} offers a greeting in ${this.language}.`;
-  }
+  return `${this.name} offers a greeting in ${this.language}.`;
 };
 
 
