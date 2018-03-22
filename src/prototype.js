312d331d8a9fdb5f702b3207ea5428eaf4079c86
Shawn Stewart <s.stewart0220@gmail.com>
2018-03-22 15:00:37 -0500

    Made progress on prototype.js & this.js

--- src/prototype.js
+++ src/prototype.js
@@ -25,29 +25,63 @@
   Inheritance chain: Humanoid -> NPC -> GameObject
   Instances of Humanoid should have all of the same properties as NPC and GameObject.
   Instances of NPC should have all of the same properties as GameObject.
-
-  Example:
-
-  const hamsterHuey = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    hp: 5,
-    name: 'Hamster Huey',
-    faction: 'Gooey Kablooie',
-    weapons: [
-      'bubblegum',
-    ],
-    language: 'Hamsterish',
-  });
-
-  hamsterHuey.greet(); // returns 'Hamster Huey offers a greeting in Hamsterish'
-  hamsterHuey.takeDamage(); // returns 'Hamster Huey took damage.'
-  hamsterHuey.destroy(); // returns 'Game object was removed from the game.'
 */
+
+class GameObject {
+  constructor(options) {
+    this.createdAt = options.createdAt;
+    this.dimensions = options.dimensions;
+  }
+
+  destroy() {
+    return 'Game object was removed from the game.';
+  }
+}
+
+class NPC extends GameObject {
+  constructor(options) {
+    super(options);
+    this.hp = options.hp;
+    this.name = options.name;
+  }
+
+  takeDamage() {
+    return `${this.name} took damage!`;
+  }
+}
+
+class Humanoid extends NPC {
+  constructor(options) {
+    super(options)
+    this.faction = options.faction;
+    this.weapons = options.weapons;
+    this.language = options.language;
+  }
+
+  greet() {
+    return `${this.name} offers a greeting in ${this.language}.`
+  }
+}
+
+// const hamsterHuey = new Humanoid({
+//   createdAt: new Date(),
+//   dimensions: {
+//     length: 2,
+//     width: 1,
+//     height: 1,
+//   },
+//   hp: 5,
+//   name: 'Hamster Huey',
+//   faction: 'Gooey Kablooie',
+//   weapons: [
+//     'bubblegum',
+//   ],
+//   language: 'Hamsterish',
+// });
+
+// console.log(hamsterHuey.greet()); // returns 'Hamster Huey offers a greeting in Hamsterish'
+// console.log(hamsterHuey.takeDamage()); // returns 'Hamster Huey took damage.'
+// console.log(hamsterHuey.destroy()); // returns 'Game object was removed from the game.'
 
 /* eslint-disable no-undef */
 
