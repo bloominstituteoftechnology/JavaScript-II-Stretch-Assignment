ee148c6d4eb46326c03ec4c4b5c139417a9633ac
BUDDHAPLEX <ericandrade@gmail.com>
2018-03-22 09:25:21 -0400

    fibonacci completed but not passing test.

--- src/recursion.js
+++ src/recursion.js
@@ -3,7 +3,12 @@
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  if (n <= 1) {
+    return n;
+  }
+  return nFibonacci(n - 1) + nFibonacci(n - 2);
 };
+
 
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
@@ -14,12 +19,4 @@ const nFactorial = (n) => {
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
-};
-
-/* eslint-enable no-unused-vars */
-
-module.exports = {
-  nFibonacci,
-  nFactorial,
-  checkMatchingLeaves,
 };
