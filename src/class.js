2a26b806c0aefe96d2cfb374a1250b08bcdb4ef2
Justin M <jekm321@gmail.com>
2018-03-22 17:33:52 -0400

    finished class, finished JS-II

--- src/class.js
+++ src/class.js
@@ -8,7 +8,16 @@
 // Return true if the potential password matches the `password` property.  Otherwise return false.
 
 // code here
+class User {
+  constructor(options) {
+    this.email = options.email;
+    this.password = options.password;
+  }
 
+  comparePasswords(potentialPassword) {
+    return this.password === potentialPassword;
+  }
+}
 // Part 2
 // Create a class called `Animal` and a class called `Cat` using ES6 classes.
 // `Cat` should extend the `Animal` class.
@@ -20,6 +29,26 @@
 // property set on the Cat instance.
 
 // code here
+class Animal {
+  constructor(options) {
+    this.age = options.age;
+  }
+
+  growOlder () {
+    return ++this.age;
+  }
+}
+
+class Cat extends Animal {
+  constructor(options) {
+    super(options);
+    this.name = options.name;
+  }
+
+  meow() {
+    return `${this.name} meowed!`;
+  }
+}
 
 /* eslint-disable no-undef */
 
