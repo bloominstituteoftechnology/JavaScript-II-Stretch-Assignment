86e6b5d6d055e4cd52169d938a4f12ac7624743f
Justin Hong <jhong2009@gmail.com>
2018-03-22 13:01:56 -0700

    created an instance of Humanoid and fixed test errors

--- src/prototype.js
+++ src/prototype.js
@@ -11,20 +11,35 @@
   // parent class
   class GameObject {
     constructor(options) {
-      this.createdAt: options.createdAt;
-      this.dimensions: options.dimensions;
+      this.createdAt = options.createdAt;
+      this.dimensions = options.dimensions;
     }
-    destroy() {
-      return `Game object was removed from the game.`;
-    }
+    // destroy() {
+    //   return `${this.name} was removed from the game.`;
+    // }
   }
+  GameObject.prototype.destroy = function destroy() {
+    return 'Game object was removed from the game.';
+  };
+
 /*
   NPC
     hp
     name
     takeDamage() // prototype method -> returns the string '<object name> took damage.'
     // should inherit destroy() from GameObject's prototype
-
+*/
+  class NPC extends GameObject {
+    constructor(NPCoptions) {
+      super(NPCoptions);
+      this.hp = NPCoptions.hp;
+      this.name = NPCoptions.name;
+    }
+    takeDamage() {
+      return `${this.name} took damage.`;
+    }
+  }
+/*
   Humanoid
     faction
     weapons
@@ -32,11 +47,47 @@
     greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
     // should inherit destroy() from GameObject through NPC
     // should inherit takeDamage() from NPC
+*/
 
+  class Humanoid extends NPC {
+    constructor(Humanoidoptions) {
+      super(Humanoidoptions);
+      this.faction = Humanoidoptions.faction;
+      this.weapons = Humanoidoptions.weapons;
+      this.language = Humanoidoptions.language;
+    }
+    greet() {
+      return `${this.name} offers a greeting in ${this.language}.`;
+    }
+  }
+
+/*
   Inheritance chain: Humanoid -> NPC -> GameObject
   Instances of Humanoid should have all of the same properties as NPC and GameObject.
   Instances of NPC should have all of the same properties as GameObject.
+*/
 
+  const playerTwo = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 2,
+      height: 1,
+    },
+    hp: 10,
+    name: 'Player Two',
+    faction: 'USA',
+    weapons: [
+      'spear gun',
+    ],
+    language: 'English',
+  });
+
+  playerTwo.greet();
+  playerTwo.takeDamage();
+  playerTwo.destroy();
+
+/*
   Example:
 
   const hamsterHuey = new Humanoid({
@@ -62,8 +113,8 @@
 
 /* eslint-disable no-undef */
 
-module.exports = {
-  GameObject,
-  NPC,
-  Humanoid,
-};
+  module.exports = {
+    GameObject,
+    NPC,
+    Humanoid,
+  };
