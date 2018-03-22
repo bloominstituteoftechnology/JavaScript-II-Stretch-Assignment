f76305ff156d2fd799c05a56ca5017cb401f7133
jeffrey <jeffreyneilflynn@gmail.com>
2018-03-22 18:03:29 -0400

    got the recursion stretch!!!

--- src/recursion.js
+++ src/recursion.js
@@ -19,9 +19,29 @@ const nFactorial = (n) => {
 };
 
 /* Extra Credit */
+const referenceValue = [];
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  const ogObjectValues = Object.values(obj);
+  referenceValue.push(ogObjectValues[0]);
+  const fixedReference = referenceValue[0];
+
+  let result = true;
+  const tempValues = Object.values(obj);
+
+  tempValues.forEach((element) => {
+    if (element !== fixedReference && typeof element !== 'object') {
+      result = false;
+    }
+    if (typeof element === 'object') {
+      const thisValue = checkMatchingLeaves(element);
+      if (thisValue === false) {
+        result = false;
+      }
+    }
+  });
+  return result;
 };
 
 /* eslint-enable no-unused-vars */
