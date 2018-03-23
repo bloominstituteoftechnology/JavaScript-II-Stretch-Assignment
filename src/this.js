124d5fefae5e9f05950974dbbac775bdb9b8d9d8
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-03-23 00:58:26 -0700

    this complete

--- src/this.js
+++ src/this.js
@@ -7,10 +7,19 @@
 class User {
   constructor(options) {
     // set a username and password property on the user object that is created
+    this.username = options.username;
+    this.password = options.password;
   }
   // create a method on the User class called `checkPassword`
   // this method should take in a string and compare it to the object's password property
   // return `true` if they match, otherwise return `false`
+  checkPassword(inputPW) {
+    let test = false;
+    if (this.password === inputPW) {
+      test = true;
+    }
+    return test;
+  }
 }
 
 const me = new User({
@@ -27,13 +36,21 @@ const checkPassword = function comparePasswords(passwordToCompare) {
   // use `this` to access the object's `password` property.
   // do not modify this function's parameters
   // note that we use the `function` keyword and not `=>`
+  let test = false;
+  if (this.password === passwordToCompare) {
+    test = true;
+  }
+  return test;
 };
 
 // invoke `checkPassword` on `me` by explicitly setting the `this` context
 // use .call, .apply, and .bind
 
 // .call
+checkPassword.call(me);
 
 // .apply
+checkPassword.apply(me);
 
 // .bind
+checkPassword.apply(me);
