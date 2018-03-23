602c1d303ab7a6265f3825c6d51bbbe91c701486
odax <nr10@albion.edu>
2018-03-23 03:09:24 -0400

    completed class for JS-II

--- src/prototype.js
+++ src/prototype.js
@@ -7,13 +7,38 @@
     createdAt
     dimensions
     destroy() // prototype method -> returns the string 'Game object was removed from the game.'
+*/
 
+class GameObject {
+  constructor(gameObjectParameters) {
+    this.createdAt = gameObjectParameters.createdAt;
+    this.dimensions = gameObjectParameters.dimensions;
+  }
+  destroy() {
+    return `Game object was removed from the game`
+  }
+}
+
+/*
   NPC
     hp
     name
     takeDamage() // prototype method -> returns the string '<object name> took damage.'
     // should inherit destroy() from GameObject's prototype
+*/
 
+class NPC extends GameObject {
+  constructor(NPCParameters) {
+    super(NPCParameters);
+    this.hp = NPCParameters.hp;
+    this.name = NPCParameters.name;
+  }
+  takeDamage() {
+    return `${this.name} took damage.`
+  }
+}
+
+/*
   Humanoid
     faction
     weapons
@@ -21,7 +46,21 @@
     greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
     // should inherit destroy() from GameObject through NPC
     // should inherit takeDamage() from NPC
+*/
 
+class Humanoid extends GameObject {
+  constructor(humanoidParameters) {
+    super(humanoidParameters);
+    this.faction = humanoidParameters.faction;
+    this.weapons = humanoidParameters.weapons;
+    this.language = humanoidParameters.language;
+  }
+  greet() {
+    return `${this.name} offers a greeting in ${this.language}.`
+  }
+}
+
+/*
   Inheritance chain: Humanoid -> NPC -> GameObject
   Instances of Humanoid should have all of the same properties as NPC and GameObject.
   Instances of NPC should have all of the same properties as GameObject.
