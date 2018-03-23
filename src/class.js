220af2692fd88e2c87ed4289efd80d1fc0893ea9
SyCoDe <SyCoDe@gmail.com>
2018-03-22 22:32:22 -0600

    finished this, some of recursion & class

--- src/class.js
+++ src/class.js
@@ -8,6 +8,15 @@
 // Return true if the potential password matches the `password` property.  Otherwise return false.
 
 // code here
+class User {
+  constructor(options) {
+    this.email = options.email;
+    this.password = options.password;
+  }
+  comparePasswords(pword) {
+    return (this.password === pword);
+  }
+}
 
 // Part 2
 // Create a class called `Animal` and a class called `Cat` using ES6 classes.
@@ -20,6 +29,7 @@
 // property set on the Cat instance.
 
 // code here
+class Animal 
 
 /* eslint-disable no-undef */
 
--- src/recursion.js
+++ src/recursion.js
@@ -1,11 +1,14 @@
 // Complete the following functions.
 
 const nFibonacci = (n) => {
+  if (n <=1) return n; //base case
+  return nFibonacci(n - 1) + nFibonacci(n - 2);
   // fibonacci sequence: 1 1 2 3 5 8 13 ...
   // return the nth number in the sequence
 };
 
 const nFactorial = (n) => {
+  
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
 };
