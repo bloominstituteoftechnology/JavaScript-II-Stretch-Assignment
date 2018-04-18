68845f2cc4668935633c708c244a48a98c68f826
jesuarva <jean.ariza@jesuarva.com>
2018-04-18 02:19:06 +0200

    END recursion.js
    PENDING: 'Extra Credit'

--- src/recursion.js
+++ src/recursion.js
@@ -3,12 +3,43 @@
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  let previous = 0;
+  let current = 1;
+  let auxiliar = 0;
+  let i = 0;
+  do {
+    auxiliar = previous + current;
+    previous = current;
+    current = auxiliar;
+  } while (++i < n);
+  return current;
 };
+console.log(nFibonacci(0));
+console.log(nFibonacci(1));
+console.log(nFibonacci(2));
+console.log(nFibonacci(3));
+console.log(nFibonacci(4));
+console.log(nFibonacci(5));
+console.log(nFibonacci(6));
+console.log(nFibonacci(7));
+console.log(nFibonacci(8));
+
 
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+  let i = 2;
+  let factorial = 1;
+  do {
+    factorial *= i;
+  } while (i++ < n);
+  return factorial;
 };
+console.log(nFactorial(1));
+console.log(nFactorial(2));
+console.log(nFactorial(3));
+console.log(nFactorial(4));
+console.log(nFactorial(5));
 
 /* Extra Credit */
 const checkMatchingLeaves = (obj) => {
